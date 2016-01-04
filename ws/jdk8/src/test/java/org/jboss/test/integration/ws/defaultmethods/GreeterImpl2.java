657da943b1e09b653f8b09af87928ac32cb5147f
Panagiotis Sotiropoulos <psotirop@redhat.com>
2016-01-04 16:07:20 +0100

    [eap-additional-testsuite] : Restructuring eap-additional-testsuite.

--- ws/jdk8/src/test/java/org/jboss/test/integration/ws/defaultmethods/GreeterImpl2.java
+++
@@ -1,6 +0,0 @@
-package org.jboss.test.integration.ws.defaultmethods;
-
-import javax.jws.WebService;
-
-@WebService(targetNamespace = "http://defaultmethods.ws.integration.test.jboss.org/")
-public class GreeterImpl2 implements GreeterSEI {}
