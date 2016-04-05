be8341678fc0742fdda2441dbfd753b40aefced0
Panagiotis Sotiropoulos <psotirop@redhat.com>
2016-04-05 15:26:00 +0200

    [jboss-additional-testsuite] : Adding the wildfly-unmerged server for adding testcases when fixes are not merged. Also adding a testcase that prooves that all the requests are processed when using the new JBossThreadPoolExecutor which reuses idle threads

---
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/web/threads/SimpleServlet.java
@@ -0,0 +1,52 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright (c) 2016, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.additional.testsuite.jdkall.web.threads;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+/**
+ */
+@WebServlet(name = "SimpleServlet", urlPatterns = {"/testNewThreadPool"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly-Unmerged/web/src/main/java"})
+public class SimpleServlet extends HttpServlet {
+
+    private static int value = 0;
+    private static Object thisLock = new Object();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+   
+        Writer writer = resp.getWriter();
+       
+        synchronized(this) {
+            value = value + 1;
+            writer.write("" + value);
+        }
+    }
+}
