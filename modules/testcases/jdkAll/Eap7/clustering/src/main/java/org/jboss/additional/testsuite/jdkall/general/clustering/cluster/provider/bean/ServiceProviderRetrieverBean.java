d02b7718e8a6ccb347ed528885d8f2b1cc38ab0b
Panagiotis Sotiropoulos <psotirop@redhat.com>
2016-04-15 15:34:12 +0200

    [jboss-additional-testsuite] : Adding some clustering tests for Eap7 and Wildfly servers. Restructuring of code in order to support also specific tests from previous servers that are not supported any more.

---
+++ modules/testcases/jdkAll/Eap7/clustering/src/main/java/org/jboss/additional/testsuite/jdkall/general/clustering/cluster/provider/bean/ServiceProviderRetrieverBean.java
@@ -0,0 +1,33 @@
+package org.jboss.additional.testsuite.jdkall.general.clustering.cluster.provider.bean;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import javax.ejb.EJB;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+import org.wildfly.clustering.group.Node;
+import org.wildfly.clustering.provider.ServiceProviderRegistration;
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+@Stateless
+@Remote(ServiceProviderRetriever.class)
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/clustering/src/main/java","modules/testcases/jdkAll/Wildfly-Release/clustering/src/main/java","modules/testcases/jdkAll/Eap7/clustering/src/main/java"})
+public class ServiceProviderRetrieverBean implements ServiceProviderRetriever {
+
+    @EJB
+    private ServiceProviderRegistration<String> registration;
+
+    @Override
+    public Collection<String> getProviders() {
+        Set<Node> nodes = this.registration.getProviders();
+        List<String> result = new ArrayList<>(nodes.size());
+        for (Node node: nodes) {
+            result.add(node.getName());
+        }
+        return result;
+    }
+}
