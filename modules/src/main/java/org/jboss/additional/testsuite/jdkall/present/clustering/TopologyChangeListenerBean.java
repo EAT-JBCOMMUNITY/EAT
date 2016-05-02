989a1f6c4eaa0b1d307f2abfcfa59e4ef11b9a79
Panagiotis Sotiropoulos <psotirop@redhat.com>
2016-05-02 13:08:43 +0200

    [Eap-Additional-Testsuite] : Updating the sructure to include the jboss features of the servers of past, the present servers and the servers of the future ....

---
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/clustering/TopologyChangeListenerBean.java
@@ -0,0 +1,104 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.additional.testsuite.jdkall.present.clustering;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+import org.infinispan.Cache;
+import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.notifications.Listener;
+import org.infinispan.notifications.cachelistener.annotation.TopologyChanged;
+import org.infinispan.notifications.cachelistener.event.TopologyChangedEvent;
+import org.infinispan.remoting.transport.Address;
+import org.jboss.as.clustering.msc.ServiceContainerHelper;
+import org.jboss.as.server.CurrentServiceContainer;
+import org.jboss.msc.service.ServiceName;
+import org.jboss.msc.service.ServiceRegistry;
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+/**
+ * EJB that establishes a stable topology.
+ * @author Paul Ferraro
+ */
+@Stateless
+@Remote(TopologyChangeListener.class)
+@Listener(sync = false)
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/clustering/src/main/java","modules/testcases/jdkAll/Wildfly-Release/clustering/src/main/java","modules/testcases/jdkAll/Eap7/clustering/src/main/java"})
+public class TopologyChangeListenerBean implements TopologyChangeListener {
+
+    @Override
+    public void establishTopology(String containerName, String cacheName, long timeout, String... nodes) throws InterruptedException {
+        Set<String> expectedMembers = new TreeSet<>(Arrays.asList(nodes));
+        ServiceRegistry registry = CurrentServiceContainer.getServiceContainer();
+        ServiceName name = ServiceName.JBOSS.append("infinispan", containerName);
+        EmbeddedCacheManager cacheContainer = ServiceContainerHelper.findValue(registry, name);
+        if (cacheContainer == null) {
+            throw new IllegalStateException(String.format("Failed to locate %s", name));
+        }
+        Cache cache = cacheContainer.getCache(cacheName);
+        if (cache == null) {
+            throw new IllegalStateException(String.format("Cache %s not found", cacheName));
+        }
+        cache.addListener(this);
+        try
+        {
+            long start = System.currentTimeMillis();
+            long now = start;
+            long endTime = start + timeout;
+            synchronized (this) {
+                Set<String> members = getMembers(cache);
+                while (!expectedMembers.equals(members)) {
+                    System.out.println(String.format("%s != %s, waiting for a topology change event...", expectedMembers, members));
+                    this.wait(endTime - now);
+                    now = System.currentTimeMillis();
+                    if (now >= endTime) {
+                        throw new InterruptedException(String.format("Cache %s/%s failed to establish view %s within %d ms.  Current view is: %s", containerName, cacheName, expectedMembers, timeout, members));
+                    }
+                    members = getMembers(cache);
+                }
+                System.out.println(String.format("Cache %s/%s successfully established view %s within %d ms.", containerName, cacheName, expectedMembers, now - start));
+            }
+        } finally {
+            cache.removeListener(this);
+        }
+    }
+
+    private static Set<String> getMembers(Cache<?, ?> cache) {
+        Set<String> members = new TreeSet<>();
+        for (Address address: cache.getAdvancedCache().getComponentRegistry().getStateTransferManager().getCacheTopology().getMembers()) {
+            members.add(address.toString());
+        }
+        return members;
+    }
+
+    @TopologyChanged
+    public void topologyChanged(TopologyChangedEvent<?, ?> event) {
+        synchronized (this) {
+            this.notify();
+        }
+    }
+}
---
+++ modules/testcases/jdkAll/Eap/clustering/src/main/java/org/jboss/additional/testsuite/jdkall/past/eap_6_4_x/clustering/CurrentTopologyServlet.java
@@ -0,0 +1,51 @@
+package org.jboss.additional.testsuite.jdkall.past.eap_6_4_x.clustering;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Set;
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+/**
+ * Utility servlet that provides information on current members of the cluster.
+ *
+ * @author Tomas Hofman (thofman@redhat.com)
+ */
+@WebServlet(urlPatterns = {CurrentTopologyServlet.SERVLET_PATH})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Eap/clustering/src/main/java"})
+public class CurrentTopologyServlet extends HttpServlet {
+
+    private static final String SERVLET_NAME = "topology";
+    public static final String SERVLET_PATH = "/" + SERVLET_NAME;
+    public static final String CLUSTER = "cluster";
+    public static final String MEMBERS = "members";
+
+    @EJB
+    CurrentTopology currentTopologyBean;
+
+    public static URI createURI(URL baseURL, String cluster) throws URISyntaxException {
+        StringBuilder builder = new StringBuilder(baseURL.toURI().resolve(SERVLET_NAME).toString());
+        builder.append('?').append(CLUSTER).append('=').append(cluster);
+        return URI.create(builder.toString());
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String cluster = req.getParameter(CLUSTER);
+        if (cluster == null) {
+            throw new ServletException(String.format("No '%s' parameter specified", CLUSTER));
+        }
+        Set<String> clusterMembers = this.currentTopologyBean.getClusterMembers(cluster);
+        for (String member: clusterMembers) {
+            resp.getWriter().write(member);
+            resp.getWriter().write(",");
+        }
+    }
+}
