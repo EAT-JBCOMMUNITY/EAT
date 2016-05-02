4773658893a82ead057d4bf1f8e29cfdcd90edb1
Panagiotis Sotiropoulos <psotirop@redhat.com>
2016-05-02 13:08:43 +0200

    [Eap-Additional-Testsuite] : Updating the sructure to include the jboss features of the servers of past, the present servers and the servers of the future ....

---
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/past/eap_6_4_x/clustering/single/web/SimpleServlet.java
@@ -0,0 +1,103 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.additional.testsuite.jdkall.past.eap_6_4_x.clustering.single.web;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+/**
+ * @author Paul Ferraro
+ */
+@WebServlet(urlPatterns = {"/simple"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Eap/clustering/src/main/java"})
+public class SimpleServlet extends HttpServlet {
+    private static final long serialVersionUID = -592774116315946908L;
+    public static final String REQUEST_DURATION_PARAM = "requestduration";
+    public static final String HEADER_SERIALIZED = "serialized";
+    public static final String URL = "simple";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(true);
+        Custom custom = (Custom) session.getAttribute("test");
+        if (custom == null) {
+            custom = new Custom(1);
+            session.setAttribute("test", custom);
+        } else {
+            custom.increment();
+        }
+        resp.setIntHeader("value", custom.getValue());
+        resp.setHeader(HEADER_SERIALIZED, Boolean.toString(custom.wasSerialized()));
+
+        // Long running request?
+        if (req.getParameter(REQUEST_DURATION_PARAM) != null) {
+            int duration = Integer.valueOf(req.getParameter(REQUEST_DURATION_PARAM));
+            try {
+                Thread.sleep(duration);
+            } catch (InterruptedException ex) {
+            }
+        }
+
+        resp.getWriter().write("Success");
+    }
+
+    public static class Custom implements Serializable {
+        private static final long serialVersionUID = -5129400250276547619L;
+        private transient boolean serialized = false;
+        private int value;
+
+        public Custom(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return this.value;
+        }
+
+        public void increment() {
+            this.value += 1;
+        }
+
+        public boolean wasSerialized() {
+            return this.serialized;
+        }
+
+        private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+            out.defaultWriteObject();
+            this.serialized = true;
+        }
+
+        private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+            in.defaultReadObject();
+            this.serialized = true;
+        }
+    }
+}
---
+++ modules/testcases/jdkAll/Eap7/clustering/src/main/java/org/jboss/additional/testsuite/jdkall/present/clustering/cluster/provider/ServiceProviderRegistrationTestCase.java
@@ -0,0 +1,73 @@
+package org.jboss.additional.testsuite.jdkall.present.clustering.cluster.provider;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.container.test.api.TargetsContainer;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.additional.testsuite.jdkall.present.clustering.EJBClientContextSelector;
+import org.jboss.additional.testsuite.jdkall.present.clustering.cluster.ClusterAbstractTestCase;
+import org.jboss.additional.testsuite.jdkall.present.clustering.cluster.provider.bean.ServiceProviderRetriever;
+import org.jboss.additional.testsuite.jdkall.present.clustering.cluster.provider.bean.ServiceProviderRetrieverBean;
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+import org.jboss.additional.testsuite.jdkall.present.clustering.ejb.EJBDirectory;
+import org.jboss.additional.testsuite.jdkall.present.clustering.ejb.RemoteEJBDirectory;
+import org.jboss.ejb.client.ContextSelector;
+import org.jboss.ejb.client.EJBClientContext;
+import org.jboss.logging.Logger;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+@RunAsClient
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/clustering/src/main/java","modules/testcases/jdkAll/Wildfly-Release/clustering/src/main/java","modules/testcases/jdkAll/Eap7/clustering/src/main/java"})
+public class ServiceProviderRegistrationTestCase extends ClusterAbstractTestCase {
+    private static final Logger log = Logger.getLogger(ServiceProviderRegistrationTestCase.class);
+    private static final String MODULE_NAME = "service-provider-registration";
+    private static final String CLIENT_PROPERTIES = "cluster/ejb3/stateless/jboss-ejb-client.properties";
+
+    @Deployment(name = DEPLOYMENT_1, managed=false, testable=false)
+    @TargetsContainer(CONTAINER_1)
+    public static Archive<?> createDeploymentForContainer1() {
+        return createDeployment();
+    }
+
+    @Deployment(name = DEPLOYMENT_2, managed=false, testable=false)
+    @TargetsContainer(CONTAINER_2)
+    public static Archive<?> createDeploymentForContainer2() {
+        return createDeployment();
+    }
+
+    private static Archive<?> createDeployment() {
+        final JavaArchive ejbJar = ShrinkWrap.create(JavaArchive.class, MODULE_NAME + ".jar");
+        ejbJar.addPackage(ServiceProviderRetriever.class.getPackage());
+        log.info(ejbJar.toString(true));
+        return ejbJar;
+    }
+
+    @Test
+    public void test() throws Exception {
+
+        ContextSelector<EJBClientContext> selector = EJBClientContextSelector.setup(CLIENT_PROPERTIES);
+
+        try (EJBDirectory directory = new RemoteEJBDirectory(MODULE_NAME)) {
+            ServiceProviderRetriever bean = directory.lookupStateless(ServiceProviderRetrieverBean.class, ServiceProviderRetriever.class);
+            Collection<String> names = bean.getProviders();
+            assertEquals(2, names.size());
+            assertTrue(names.toString(), names.contains(NODE_1));
+            assertTrue(names.toString(), names.contains(NODE_2));
+        } finally {
+            // reset the selector
+            if (selector != null) {
+                EJBClientContext.setSelector(selector);
+            }
+        }
+    }
+}
