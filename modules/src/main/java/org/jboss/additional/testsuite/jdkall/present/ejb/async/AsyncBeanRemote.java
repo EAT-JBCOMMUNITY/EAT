ead2b87d71be75924f725b67c43bff79a7bb7bd3
Panagiotis Sotiropoulos <psotirop@redhat.com>
2016-05-16 15:33:16 +0200

    [eap-additional-testsuite] : Adding the ejb sub-component level plus one some ejb sample tests.

---
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/async/AsyncBeanRemote.java
@@ -0,0 +1,46 @@
+package org.jboss.additional.testsuite.jdkall.present.ejb.async;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import javax.ejb.AsyncResult;
+import javax.ejb.Asynchronous;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+/**
+ * Bean with asynchronous methods.
+ * 
+ * @author Ondrej Chaloupka
+ */
+@Stateless
+@Asynchronous
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Eap7/ejb/src/main/java","modules/testcases/jdkAll/Wildfly-Release/ejb/src/main/java","modules/testcases/jdkAll/Wildfly/ejb/src/main/java","modules/testcases/jdkAll/Eap/ejb/src/main/java"})
+public class AsyncBeanRemote implements AsyncBeanRemoteInterface {   
+    @EJB
+    AsyncBean asyncBean;
+
+    @Override
+    public void asyncMethod() throws InterruptedException {
+        AsyncBean.voidMethodCalled = false;
+        final CountDownLatch latch = new CountDownLatch(1);
+        final CountDownLatch latch2 = new CountDownLatch(1);
+        asyncBean.asyncMethod(latch, latch2);
+        latch.countDown();
+        latch2.await();
+        if(!AsyncBean.voidMethodCalled) {
+            throw new IllegalArgumentException("voidMethodCalled");
+        }
+    }
+
+    @Override
+    public Future<Boolean> futureMethod() throws InterruptedException, ExecutionException {
+        AsyncBean.futureMethodCalled = false;
+        final CountDownLatch latch = new CountDownLatch(1);
+        final Future<Boolean> future = asyncBean.futureMethod(latch);
+        latch.countDown();
+        return new AsyncResult<Boolean>(future.get());
+    }  
+}
