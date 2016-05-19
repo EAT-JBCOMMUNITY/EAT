5e9f886c5b9f12f376ce475836fad3f7029da613
Panagiotis Sotiropoulos <psotirop@redhat.com>
2016-05-19 15:41:40 +0200

    Correction of a package name

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/web/threads/SimpleServlet.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/web/threads/SimpleServlet.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.additional.testsuite.jdkall.general.web.threads;
+package org.jboss.additional.testsuite.jdkall.present.web.threads;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
