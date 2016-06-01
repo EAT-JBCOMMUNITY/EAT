0b7a85020c47e4fa413f165b54a132595b00232c
Panagiotis Sotiropoulos <psotirop@redhat.com>
2016-06-01 12:52:01 +0200

    [eap-additional-testsuite] : Adding the logging sub-component level plus some logging sample tests.

---
+++ modules/testcases/jdk8/Wildfly-Release/logging/src/main/java/org/jboss/additional/testsuite/jdk8/present/logging/Dummy.java
@@ -0,0 +1,10 @@
+package org.jboss.additional.testsuite.jdk8.present.logging;
+
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+/**
+ * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
+ */
+@EapAdditionalTestsuite({"modules/testcases/jdk8/Eap7/logging/src/main/java","modules/testcases/jdk8/Wildfly-Release/logging/src/main/java","modules/testcases/jdk8/Wildfly/logging/src/main/java","modules/testcases/jdk8/Eap/logging/src/main/java"})
+public class Dummy {
+}
