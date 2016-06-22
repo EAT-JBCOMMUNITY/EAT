3102679ad4bbcca610e205c45ca1469a70fba5d2
Panagiotis Sotiropoulos <psotirop@redhat.com>
2016-06-22 15:47:12 +0200

    [eap-additional-testsuite] : Renaming of eap server to eap64x. Addition of eap 6.3.x server.

---
+++ modules/testcases/jdkAll/Eap64x/ejb/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/async/AsyncChildBean.java
@@ -0,0 +1,19 @@
+package org.jboss.additional.testsuite.jdkall.present.ejb.async;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import javax.ejb.Stateless;
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+@Stateless
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Eap7/ejb/src/main/java","modules/testcases/jdkAll/Wildfly-Release/ejb/src/main/java","modules/testcases/jdkAll/Wildfly/ejb/src/main/java","modules/testcases/jdkAll/Eap64x/ejb/src/main/java","modules/testcases/jdkAll/Eap64x-Proposed/ejb/src/main/java"})
+public class AsyncChildBean extends AsyncParentClass {
+
+    public void asyncMethod(CountDownLatch latch, CountDownLatch latch2) throws InterruptedException {
+        latch.await(5, TimeUnit.SECONDS);
+        voidMethodCalled = true;
+        latch2.countDown();
+    }
+    
+}
---
+++ modules/testcases/jdkAll/Eap64x/web/src/main/java/org/jboss/additional/testsuite/jdkall/present/web/classloading/war/WebInfLibClass.java
@@ -0,0 +1,28 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2016, Red Hat Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.additional.testsuite.jdkall.present.web.classloading.war;
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/web/src/main/java","modules/testcases/jdkAll/Eap7/web/src/main/java","modules/testcases/jdkAll/Eap64x/web/src/main/java","modules/testcases/jdkAll/Eap64x-Proposed/web/src/main/java","modules/testcases/jdkAll/Wildfly-Release/web/src/main/java"})
+public class WebInfLibClass {
+
+}
