2de425495d578e36117d4244c892397e18554d22
Panagiotis Sotiropoulos <psotirop@redhat.com>
2017-07-18 16:32:47 +0200

    [EAT] : Adding ApacheHttpClient43TestCase in EAT.

---
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/jaxrs/client/resource/ApacheHttpClient4ResourceImpl.java
@@ -0,0 +1,37 @@
+package org.jboss.additional.testsuite.jdkall.present.jaxrs.client.resource;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jboss.resteasy.spi.NoLogWebApplicationException;
+import org.jboss.resteasy.util.HttpResponseCodes;
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/jaxrs/src/main/java","modules/testcases/jdkAll/Eap7/jaxrs/src/main/java","modules/testcases/jdkAll/Eap70x/jaxrs/src/main/java"})
+public class ApacheHttpClient4ResourceImpl implements ApacheHttpClient4Resource {
+    public String get() {
+    /*    try {
+            Thread.sleep(7000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }*/
+        return "hello world";
+    }
+
+    public String error() {
+    /*    try {
+            Thread.sleep(7000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }*/
+        throw new NoLogWebApplicationException(HttpResponseCodes.SC_NOT_FOUND);
+    }
+
+    public String getData(String data) {
+    /*    try {
+            Thread.sleep(7000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }*/
+        return "Here is your string:" + data;
+    }
+}
