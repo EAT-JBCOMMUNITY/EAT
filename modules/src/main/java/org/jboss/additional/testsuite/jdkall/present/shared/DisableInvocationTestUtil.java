5a978f165730b495a7d527d3858843ca0adf6a6c
Panagiotis Sotiropoulos <psotirop@redhat.com>
2017-07-28 10:48:18 +0200

    [EAT] : Fixing the testcase for AsyncMethodTestCase. Removing not needed dependencies.

---
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/shared/DisableInvocationTestUtil.java
@@ -0,0 +1,13 @@
+package  org.jboss.additional.testsuite.jdkall.present.shared;
+
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+/**
+ * @author Kabir Khan
+ */
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Eap7/ejb/src/main/java","modules/testcases/jdkAll/Wildfly/ejb/src/main/java"})
+public class DisableInvocationTestUtil {
+    public static void disable() {
+        AssumeTestGroupUtil.assumeInvocationTestsEnabled();
+    }
+}
