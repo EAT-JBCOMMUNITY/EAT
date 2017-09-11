ce7e7149b5c4f0cef7f686be9a5eb12b06b6fa1f
panossot <panos@localhost.localdomain>
2017-09-11 10:42:07 +0200

    [EAT]: Enabling testcase of JBEAP-11897 for Eap 70x

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/web/servlet/headers/CookieHeaderServlet.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/web/servlet/headers/CookieHeaderServlet.java
@@ -24,8 +24,6 @@ package org.jboss.additional.testsuite.jdkall.present.web.servlet.headers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -34,7 +32,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
 
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/web/src/main/java","modules/testcases/jdkAll/Eap7/web/src/main/java"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/web/src/main/java","modules/testcases/jdkAll/Eap7/web/src/main/java","modules/testcases/jdkAll/Eap70x/web/src/main/java"})
 @WebServlet(name = "CookieHeaderServlet", urlPatterns = {"/cookieHeaderServlet"})
 public class CookieHeaderServlet extends HttpServlet {
 
