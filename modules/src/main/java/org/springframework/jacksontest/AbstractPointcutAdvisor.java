516a7a62fb5ac3dc09626ef1818f7e5ea5a3386c
panossot <psotirop@redhat.com>
2018-01-04 17:04:04 +0100

    [EAT] : Updating the references servers for some classes of the previous commit.

--- modules/src/main/java/org/springframework/jacksontest/AbstractPointcutAdvisor.java
+++ modules/src/main/java/org/springframework/jacksontest/AbstractPointcutAdvisor.java
@@ -2,7 +2,7 @@ package org.springframework.jacksontest;
 
 import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
 
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/jaxrs/src/main/java","modules/testcases/jdkAll/Eap7/jaxrs/src/main/java","modules/testcases/jdkAll/Eap7.1.0.Beta/jaxrs/src/main/java","modules/testcases/jdkAll/Eap70x/jaxrs/src/main/java"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/jaxrs/src/main/java","modules/testcases/jdkAll/Eap7/jaxrs/src/main/java","modules/testcases/jdkAll/Eap71x/jaxrs/src/main/java","modules/testcases/jdkAll/Eap70x/jaxrs/src/main/java"})
 public class AbstractPointcutAdvisor {
 
 }
--- modules/src/main/java/org/springframework/jacksontest/BogusPointcutAdvisor.java
+++ modules/src/main/java/org/springframework/jacksontest/BogusPointcutAdvisor.java
@@ -3,7 +3,7 @@ package org.springframework.jacksontest;
 import javax.xml.bind.annotation.XmlRootElement;
 import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
 
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/jaxrs/src/main/java","modules/testcases/jdkAll/Eap7/jaxrs/src/main/java","modules/testcases/jdkAll/Eap7.1.0.Beta/jaxrs/src/main/java","modules/testcases/jdkAll/Eap70x/jaxrs/src/main/java"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/jaxrs/src/main/java","modules/testcases/jdkAll/Eap7/jaxrs/src/main/java","modules/testcases/jdkAll/Eap71x/jaxrs/src/main/java","modules/testcases/jdkAll/Eap70x/jaxrs/src/main/java"})
 @XmlRootElement
 public class BogusPointcutAdvisor extends AbstractPointcutAdvisor {
     public BogusPointcutAdvisor() {
