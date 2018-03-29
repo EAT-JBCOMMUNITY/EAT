1501c1201c914dfac16e2086649d1fedbd1b2862
panossot <psotirop@redhat.com>
2018-03-29 12:12:06 +0200

    Adding test in EAT for JBEAP-14505 in order to be tested with all the servers

---
+++ modules/src/main/java/com/mchange/v2/c3p0/jacksonTest/ComboPooledDataSource.java
@@ -0,0 +1,8 @@
+package com.mchange.v2.c3p0.jacksonTest;
+
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/jaxrs/src/main/java","modules/testcases/jdkAll/Eap7/jaxrs/src/main/java","modules/testcases/jdkAll/Eap71x-Proposed/jaxrs/src/main/java","modules/testcases/jdkAll/Eap71x/jaxrs/src/main/java"})
+public class ComboPooledDataSource {
+
+}
