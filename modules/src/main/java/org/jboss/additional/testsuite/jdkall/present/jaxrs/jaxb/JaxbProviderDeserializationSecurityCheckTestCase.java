d8ca383af48f8b37b050743ac9fb14dcbd584e82
Panagiotis Sotiropoulos <psotirop@redhat.com>
2018-04-24 18:17:34 +0200

    Upgrade version for eap 7.1.3 pr is merged without the fix to be included.
    Upgrade version for eap 7.1.3 pr is merged without the fix to be included.

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/jaxrs/jaxb/JaxbProviderDeserializationSecurityCheckTestCase.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/jaxrs/jaxb/JaxbProviderDeserializationSecurityCheckTestCase.java
@@ -117,7 +117,7 @@ public class JaxbProviderDeserializationSecurityCheckTestCase {
 
     }
     
-    @ATTest({"modules/testcases/jdkAll/Wildfly/jaxrs/src/main/java#13.0.0.Alpha1","modules/testcases/jdkAll/Eap71x/jaxrs/src/main/java#7.1.3"})
+    @ATTest({"modules/testcases/jdkAll/Wildfly/jaxrs/src/main/java#13.0.0.Alpha1","modules/testcases/jdkAll/Eap71x/jaxrs/src/main/java#7.1.33"})
     public void testMChangeV2C3p0() throws Exception {
         String result = performCall("rest/jaxb/mchange");
 
