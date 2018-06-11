c87d4d42977d3c03855ef6a0710156931bdfaaf9
panossot <psotirop@redhat.com>
2018-06-11 11:38:51 +0200

    Updating TopologyChangeListenerBean

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/past/wildfly/clustering/TopologyChangeListenerBean.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/past/wildfly/clustering/TopologyChangeListenerBean.java
@@ -21,24 +21,23 @@
  */
 package org.jboss.additional.testsuite.jdkall.present.clustering;
 
+import java.util.Arrays;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.TreeSet;
 
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 
 import org.infinispan.Cache;
-import org.infinispan.distribution.DistributionManager;
-import org.infinispan.distribution.LocalizedCacheTopology;
+import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.notifications.Listener;
 import org.infinispan.notifications.cachelistener.annotation.TopologyChanged;
 import org.infinispan.notifications.cachelistener.event.TopologyChangedEvent;
+import org.infinispan.remoting.transport.Address;
+import org.jboss.as.clustering.msc.ServiceContainerHelper;
 import org.jboss.as.server.CurrentServiceContainer;
-import org.jboss.logging.Logger;
 import org.jboss.msc.service.ServiceName;
-import org.wildfly.clustering.infinispan.spi.InfinispanCacheRequirement;
-import org.wildfly.clustering.service.PassiveServiceSupplier;
+import org.jboss.msc.service.ServiceRegistry;
 import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
 
 /**
@@ -48,55 +47,58 @@ import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
 @Stateless
 @Remote(TopologyChangeListener.class)
 @Listener(sync = false)
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/clustering/src/main/java#14.0.0"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/clustering/src/main/java#9.0.0*13.0.0","modules/testcases/jdkAll/WildflyRelease/clustering/src/main/java","modules/testcases/jdkAll/Eap7/clustering/src/main/java","modules/testcases/jdkAll/Eap71x-Proposed/clustering/src/main/java","modules/testcases/jdkAll/Eap71x/clustering/src/main/java","modules/testcases/jdkAll/Eap7.1.0.Beta/clustering/src/main/java","modules/testcases/jdkAll/Eap70x/clustering/src/main/java","modules/testcases/jdkAll/Eap70x-Proposed/clustering/src/main/java"})
 public class TopologyChangeListenerBean implements TopologyChangeListener {
-
-    private static final Logger logger = Logger.getLogger(TopologyChangeListenerBean.class);
 
     @Override
     public void establishTopology(String containerName, String cacheName, long timeout, String... nodes) throws InterruptedException {
-        Set<String> expectedMembers = Stream.of(nodes).sorted().collect(Collectors.toSet());
-        ServiceName name = ServiceName.parse(InfinispanCacheRequirement.CACHE.resolve(containerName, cacheName));
-        Cache<?, ?> cache = new PassiveServiceSupplier<Cache<?, ?>>(CurrentServiceContainer.getServiceContainer(), name).get();
+        Set<String> expectedMembers = new TreeSet<>(Arrays.asList(nodes));
+        ServiceRegistry registry = CurrentServiceContainer.getServiceContainer();
+        ServiceName name = ServiceName.JBOSS.append("infinispan", containerName);
+        EmbeddedCacheManager cacheContainer = ServiceContainerHelper.findValue(registry, name);
+        if (cacheContainer == null) {
+            throw new IllegalStateException(String.format("Failed to locate %s", name));
+        }
+        Cache cache = cacheContainer.getCache(cacheName);
         if (cache == null) {
-            throw new IllegalStateException(String.format("Cache %s not found", name));
+            throw new IllegalStateException(String.format("Cache %s not found", cacheName));
         }
         cache.addListener(this);
-        try {
+        try
+        {
+            long start = System.currentTimeMillis();
+            long now = start;
+            long endTime = start + timeout;
             synchronized (this) {
-                DistributionManager dist = cache.getAdvancedCache().getDistributionManager();
-                LocalizedCacheTopology topology = dist.getCacheTopology();
-                Set<String> members = getMembers(topology);
-                long start = System.currentTimeMillis();
-                long now = start;
-                long endTime = start + timeout;
+                Set<String> members = getMembers(cache);
                 while (!expectedMembers.equals(members)) {
-                    logger.infof("%s != %s, waiting for a topology change event. Current topology id = %d", expectedMembers, members, topology.getTopologyId());
+                    System.out.println(String.format("%s != %s, waiting for a topology change event...", expectedMembers, members));
                     this.wait(endTime - now);
                     now = System.currentTimeMillis();
                     if (now >= endTime) {
                         throw new InterruptedException(String.format("Cache %s/%s failed to establish view %s within %d ms.  Current view is: %s", containerName, cacheName, expectedMembers, timeout, members));
                     }
-                    topology = dist.getCacheTopology();
-                    members = getMembers(topology);
+                    members = getMembers(cache);
                 }
-                logger.infof("Cache %s/%s successfully established view %s within %d ms. Topology id = %d", containerName, cacheName, expectedMembers, now - start, topology.getTopologyId());
+                System.out.println(String.format("Cache %s/%s successfully established view %s within %d ms.", containerName, cacheName, expectedMembers, now - start));
             }
         } finally {
             cache.removeListener(this);
         }
     }
 
-    private static Set<String> getMembers(LocalizedCacheTopology topology) {
-        return topology.getMembers().stream().map(Object::toString).sorted().collect(Collectors.toSet());
+    private static Set<String> getMembers(Cache<?, ?> cache) {
+        Set<String> members = new TreeSet<>();
+        for (Address address: cache.getAdvancedCache().getComponentRegistry().getStateTransferManager().getCacheTopology().getMembers()) {
+            members.add(address.toString());
+        }
+        return members;
     }
 
     @TopologyChanged
     public void topologyChanged(TopologyChangedEvent<?, ?> event) {
-        if (!event.isPre()) {
-            synchronized (this) {
-                this.notify();
-            }
+        synchronized (this) {
+            this.notify();
         }
     }
 }
