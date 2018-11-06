afa96018c4ce98907bc4bc7481384366a2fc5a0b
panossot <psotirop@redhat.com>
2018-11-06 11:47:04 +0100

    Minor correction ...

--- MINI_LAB/wildfly.sh
+++ MINI_LAB/wildfly.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-
 cd ./Dockerfiles/wildfly
 docker build -t docker.io/eat . > outputWildfly.txt
 
-sudo docker stop $(sudo docker ps -a -q)
-sudo docker rm $(sudo docker ps -a -q)
+#sudo docker stop $(sudo docker ps -a -q)
+#sudo docker rm $(sudo docker ps -a -q)
 sudo docker image rm docker.io/eat -f
 
 
