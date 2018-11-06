4d11ff5bf5d7709d784614ad6e6335466cd53c41
panossot <psotirop@redhat.com>
2018-11-06 11:19:36 +0100

    MINI_LAB - Addition of 6.4.x version ...

--- MINI_LAB/72x.sh
+++ MINI_LAB/72x.sh
@@ -3,4 +3,6 @@
 cd ./Dockerfiles/JBossServers72x
 docker build -t docker.io/eat72x . > outputEap72x.txt
 
+sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
 sudo docker image rm docker.io/eat72x -f
--- MINI_LAB/wildfly.sh
+++ MINI_LAB/wildfly.sh
@@ -4,6 +4,8 @@
 cd ./Dockerfiles/wildfly
 docker build -t docker.io/eat . > outputWildfly.txt
 
+sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
 sudo docker image rm docker.io/eat -f
 
 
