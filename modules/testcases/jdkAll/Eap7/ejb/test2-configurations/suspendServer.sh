e583a8115baa20675d7d692abc29cb9f03059c2d
panossot <psotirop@redhat.com>
2018-11-17 02:49:50 +0100

    Adding also all the text resources at the second level.

--- modules/testcases/jdkAll/Eap7/ejb/test2-configurations/suspendServer.sh
+++
@@ -1,8 +0,0 @@
-PPIDD=$(ps -aux | grep "standalone.sh" | grep "bin" | awk '{print $2}')
-echo $PPIDD
-for childprocess in $(ps -o pid --ppid $PPIDD | tail -n +2); 
-    do kill -TSTP $childprocess; 
-    echo $childprocess;
-done;
-#kill -TSTP $(ps -aux | grep "standalone.sh" | grep "bin" | awk '{print $2}')
-
--- modules/testcases/jdkAll/Eap72x/web/test-configurations/src/test/resources/test.jsp
+++
@@ -1,4 +0,0 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
-<%@ taglib tagdir="/WEB-INF/tags" prefix="tags" %>
-<tags:test text="${0}  ${fn:length ([1, 2])}"/>
