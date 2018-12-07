2e0bd318642a01ad08694e0cf748ce137134b30f
panossot <psotirop@redhat.com>
2018-12-07 10:56:03 +0100

    Enablying more tests ...

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/jaxrs/client/resource/ApacheHttpClient43ResourceImpl.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/jaxrs/client/resource/ApacheHttpClient43ResourceImpl.java
@@ -3,7 +3,7 @@ package org.jboss.additional.testsuite.jdkall.present.jaxrs.client.resource;
 import javax.ws.rs.core.Response;
 import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
 
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Eap7/jaxrs/src/main/java","modules/testcases/jdkAll/Eap72x/jaxrs/src/main/java","modules/testcases/jdkAll/Eap72x-Proposed/jaxrs/src/main/java","modules/testcases/jdkAll/Eap71x-Proposed/jaxrs/src/main/java","modules/testcases/jdkAll/Eap71x/jaxrs/src/main/java"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/jaxrs/src/main/java","modules/testcases/jdkAll/Eap7/jaxrs/src/main/java","modules/testcases/jdkAll/Eap72x/jaxrs/src/main/java","modules/testcases/jdkAll/Eap72x-Proposed/jaxrs/src/main/java","modules/testcases/jdkAll/Eap71x-Proposed/jaxrs/src/main/java","modules/testcases/jdkAll/Eap71x/jaxrs/src/main/java"})
 public class ApacheHttpClient43ResourceImpl implements ApacheHttpClient43Resource {
     public Response get() {
     /*    try {
