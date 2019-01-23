29649638a94e205e47e30251e3e375226cfc474e
panossot <psotirop@redhat.com>
2019-01-23 10:14:37 +0100

    [EAT] : jackson-databind: arbitrary code execution in jboss-common-core class , testcase for all servers ...

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/security/other/SecurityDeserializationTestCase.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/security/other/SecurityDeserializationTestCase.java
@@ -157,6 +157,25 @@ public class SecurityDeserializationTestCase {
 
     }
 
+    @ATTest({"modules/testcases/jdkAll/Wildfly/security/src/main/java#16.0.0.Beta1"})
+    public void testSecuirtyDatabind6() throws Exception {
+
+        final String JSON = aposToQuotes(
+                "{'v':['org.jboss.util.propertyeditor.DocumentEditor','/tmp/foobar.txt']}"
+                 );
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enableDefaultTyping();
+
+        try {
+            PolyWrapper sc = mapper.readValue(JSON, PolyWrapper.class);
+            fail("Should not be able to deserialize because of security prevention.");
+        }catch(JsonMappingException e){
+            assertTrue("Fail because of security issues...",e.getMessage().contains("prevented for security reasons"));
+        }
+
+    }
+
     static class SerializedClass {
 
         public int id;
