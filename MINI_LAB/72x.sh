130346cb5aa183748f5db9067b4a7938b728ac1b
Panagiotis Sotiropoulos <psotirop@redhat.com>
2019-01-24 16:35:17 +0100

    MINILAB script update for 72x

--- MINI_LAB/72x.sh
+++ MINI_LAB/72x.sh
@@ -5,4 +5,4 @@ docker build -t docker.io/eat72x . > outputEap72x.txt
 
 #sudo docker stop $(sudo docker ps -a -q)
 #sudo docker rm $(sudo docker ps -a -q)
-sudo docker image rm docker.io/eat72x -f
+sudo docker rmi -f docker.io/eat72x
