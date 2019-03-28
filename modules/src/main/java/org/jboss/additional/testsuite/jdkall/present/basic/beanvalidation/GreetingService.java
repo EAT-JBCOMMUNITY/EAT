ba96852cda4c61ef30a2acf54ad6e4c2bb7e0ba2
panossot <psotirop@redhat.com>
2019-03-28 11:06:29 +0100

    Minor update

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/basic/beanvalidation/GreetingService.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/basic/beanvalidation/GreetingService.java
@@ -4,7 +4,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.validation.constraints.NotNull;
 import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
 
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/basic/src/main/java","modules/testcases/jdkAll/Eap72x/basic/src/main/java","modules/testcases/jdkAll/Eap72x-Proposed/basic/src/main/java","modules/testcases/jdkAll/Protean/basic/releases/test-configurations/src/main/java#0.9.1**0.9.1","modules/testcases/jdkAll/Protean/basic/releases/test-configurations2/src/main/java#0.10.0"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/basic/src/main/java","modules/testcases/jdkAll/Eap72x/basic/src/main/java","modules/testcases/jdkAll/Eap72x-Proposed/basic/src/main/java","modules/testcases/jdkAll/Protean/basic/releases/test-configurations/src/main/java#0.9.1*0.9.1","modules/testcases/jdkAll/Protean/basic/releases/test-configurations2/src/main/java#0.10.0*0.12.0"})
 @ApplicationScoped
 public class GreetingService {
 
--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/basic/beanvalidation/custom/MyOtherBean.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/basic/beanvalidation/custom/MyOtherBean.java
@@ -2,7 +2,7 @@ package org.jboss.additional.testsuite.jdkall.present.basic.beanvalidation.custo
 
 import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
 
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/basic/src/main/java","modules/testcases/jdkAll/Eap72x/basic/src/main/java","modules/testcases/jdkAll/Eap72x-Proposed/basic/src/main/java","modules/testcases/jdkAll/Protean/basic/releases/test-configurations/src/main/java#0.9.1*0.9.1","modules/testcases/jdkAll/Protean/basic/releases/test-configurations2/src/main/java#0.10.0"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/basic/src/main/java","modules/testcases/jdkAll/Eap72x/basic/src/main/java","modules/testcases/jdkAll/Eap72x-Proposed/basic/src/main/java","modules/testcases/jdkAll/Protean/basic/releases/test-configurations/src/main/java#0.9.1*0.9.1","modules/testcases/jdkAll/Protean/basic/releases/test-configurations2/src/main/java#0.10.0*0.12.0"})
 @MyCustomConstraint
 public class MyOtherBean {
 
