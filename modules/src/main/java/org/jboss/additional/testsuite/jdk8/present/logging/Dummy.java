d57611a406b7b398287720b1dac9fe81c9e56f99
Lkop <loukaskopanias122@gmail.com>
2019-06-02 15:02:32 -0700

    Adding latest Wildfly Release 17.0.0.Alpha1

--- modules/src/main/java/org/jboss/additional/testsuite/jdk8/present/logging/Dummy.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdk8/present/logging/Dummy.java
@@ -5,6 +5,6 @@ import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
 /**
  * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
  */
-@EapAdditionalTestsuite({"modules/testcases/jdk8/WildflyRelease-13.0.0.Final/logging/src/main/java","modules/testcases/jdk8/Eap7/logging/src/main/java","modules/testcases/jdk8/Eap71x-Proposed/logging/src/main/java","modules/testcases/jdk8/Eap71x/logging/src/main/java","modules/testcases/jdk8/Eap70x/logging/src/main/java","modules/testcases/jdk8/Eap70x-Proposed/logging/src/main/java","modules/testcases/jdk8/WildflyRelease-10.1.0.Final/logging/src/main/java","modules/testcases/jdk8/Wildfly/logging/src/main/java"})
+@EapAdditionalTestsuite({"modules/testcases/jdk8/WildflyRelease-13.0.0.Final/logging/src/main/java","modules/testcases/jdk8/Eap7/logging/src/main/java","modules/testcases/jdk8/Eap71x-Proposed/logging/src/main/java","modules/testcases/jdk8/Eap71x/logging/src/main/java","modules/testcases/jdk8/Eap70x/logging/src/main/java","modules/testcases/jdk8/Eap70x-Proposed/logging/src/main/java","modules/testcases/jdk8/WildflyRelease-10.1.0.Final/logging/src/main/java","modules/testcases/jdk8/Wildfly/logging/src/main/java","modules/testcases/jdk8/WildflyRelease-17.0.0.Alpha1/logging/src/main/java"})
 public class Dummy {
 }
