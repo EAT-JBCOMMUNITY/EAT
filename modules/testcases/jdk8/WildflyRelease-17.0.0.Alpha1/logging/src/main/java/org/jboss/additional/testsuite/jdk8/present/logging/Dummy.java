edc1a105dc6c6b4eab35d47195d699ad7b48fe2b
Lkop <loukaskopanias122@gmail.com>
2019-06-02 15:14:53 -0700

    Adding jdk8 latest Wildfly Release 17.0.0.Alpha1

---
+++ modules/testcases/jdk8/WildflyRelease-17.0.0.Alpha1/logging/src/main/java/org/jboss/additional/testsuite/jdk8/present/logging/Dummy.java
@@ -0,0 +1,10 @@
+package org.jboss.additional.testsuite.jdk8.present.logging;
+
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+/**
+ * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
+ */
+@EapAdditionalTestsuite({"modules/testcases/jdk8/WildflyRelease-13.0.0.Final/logging/src/main/java","modules/testcases/jdk8/Eap7/logging/src/main/java","modules/testcases/jdk8/Eap71x-Proposed/logging/src/main/java","modules/testcases/jdk8/Eap71x/logging/src/main/java","modules/testcases/jdk8/Eap70x/logging/src/main/java","modules/testcases/jdk8/Eap70x-Proposed/logging/src/main/java","modules/testcases/jdk8/WildflyRelease-10.1.0.Final/logging/src/main/java","modules/testcases/jdk8/Wildfly/logging/src/main/java","modules/testcases/jdk8/WildflyRelease-17.0.0.Alpha1/logging/src/main/java"})
+public class Dummy {
+}
