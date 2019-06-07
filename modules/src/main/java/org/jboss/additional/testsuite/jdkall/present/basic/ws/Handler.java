3240487f21c4ccafd19662febf36d323d000191a
Lkop <loukaskopanias122@gmail.com>
2019-06-07 05:05:57 -0700

    Conflict fixes

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/basic/ws/Handler.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/basic/ws/Handler.java
@@ -29,7 +29,7 @@ import javax.xml.ws.handler.LogicalMessageContext;
 import javax.xml.ws.handler.MessageContext;
 
 
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/basic/src/main/java#16.0.0.Final","modules/testcases/jdkAll/WildflyRelease-17.0.0.Alpha1/basic/src/main/java","modules/testcases/jdkAll/Eap72x-Proposed/basic/src/main/java#7.2.1","modules/testcases/jdkAll/Eap72x/basic/src/main/java#7.2.1","modules/testcases/jdkAll/Eap71x-Proposed/basic/src/main/java#7.1.6","modules/testcases/jdkAll/Eap71x/basic/src/main/java#7.1.6"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/basic/src/main/java#16.0.0.Final","modules/testcases/jdkAll/Eap72x-Proposed/basic/src/main/java#7.2.1","modules/testcases/jdkAll/Eap72x/basic/src/main/java#7.2.1","modules/testcases/jdkAll/Eap71x-Proposed/basic/src/main/java#7.1.6","modules/testcases/jdkAll/Eap71x/basic/src/main/java#7.1.6"})
 public class Handler implements LogicalHandler<LogicalMessageContext> {
 
     public static boolean containsKey = false;
