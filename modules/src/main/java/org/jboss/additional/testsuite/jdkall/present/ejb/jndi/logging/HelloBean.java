0ed8dd75877c2b0223423eb16e3e404344b3c934
panossot <psotirop@redhat.com>
2019-06-11 13:02:58 +0200

    Preparation to add new server version

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/jndi/logging/HelloBean.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/jndi/logging/HelloBean.java
@@ -7,6 +7,6 @@ import javax.ejb.Stateless;
 
 @Stateless(name="Hello")
 @Remote(Hello.class)
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/ejb/src/main/java#16.0.0.Beta1","modules/testcases/jdkAll/WildflyRelease-17.0.0.Final/ejb/src/main/java", "modules/testcases/jdkAll/Eap72x/ejb/src/main/java#7.2.2", "modules/testcases/jdkAll/Eap72x-Proposed/ejb/main/java#7.2.2"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/ejb/src/main/java#16.0.0.Beta1","modules/testcases/jdkAll/WildflyRelease-17.0.0.Final/ejb/src/main/java", "modules/testcases/jdkAll/Eap72x/ejb/src/main/java#7.2.2", "modules/testcases/jdkAll/Eap72x-Proposed/ejb/main/java#7.2.2","modules/testcases/jdkAll/Eap7/ejb/src/main/java"})
 public class HelloBean implements Hello {
 }
--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/stateful/passivation/TwoPassivationStoresTestCase.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/stateful/passivation/TwoPassivationStoresTestCase.java
@@ -45,7 +45,7 @@ import javax.naming.InitialContext;
  */
 @RunWith(Arquillian.class)
 @ServerSetup(TwoPassivationStoresServerSetupTask.class)
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/ejb/src/main/java#16.0.0.Beta1","modules/testcases/jdkAll/WildflyRelease-17.0.0.Final/ejb/src/main/java", "modules/testcases/jdkAll/Eap72x/ejb/src/main/java#7.2.1", "modules/testcases/jdkAll/Eap72x-Proposed/ejb/main/java#7.2.1"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/ejb/src/main/java#16.0.0.Beta1","modules/testcases/jdkAll/WildflyRelease-17.0.0.Final/ejb/src/main/java", "modules/testcases/jdkAll/Eap72x/ejb/src/main/java#7.2.1", "modules/testcases/jdkAll/Eap72x-Proposed/ejb/main/java#7.2.1", "modules/testcases/jdkAll/Eap7/ejb/src/main/java"})
 public class TwoPassivationStoresTestCase {
 
     private static final String DEPLOYMENT = "DEPLOYMENT";
