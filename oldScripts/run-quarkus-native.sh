27b41a2da4662e56ccc2d057721661711ff2d61b
Romain Pelisse <belaran@gmail.com>
2019-07-09 12:14:52 +0200

    Fix up the run-quarkus scritps

--- oldScripts/run-quarkus-native.sh
+++ oldScripts/run-quarkus-native.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+readonly NATIVE=${NATIVE:-''} # export NATIVE=-Pnative
 
 sed -i 's/${QUARKUS_VERSION}/'"${QUARKUS_VERSION}"'/g' ./modules/testcases/jdkAll/Protean/quarkus/quark/test-configurations/pom.xml
-./Maven/apache-maven-3.5.3/bin/mvn clean install -Dquarkus -DJBOSS_VERSION=${JBOSS_VERSION} -Dquarkus-project-discovery=false -Dquarkus-cp-cache=false -Dquarkus-workspace-discovery=false -Dquarkus-classpath-cache=false
+./Maven/apache-maven-3.5.3/bin/mvn clean install -Dquarkus "${NATIVE}"-DJBOSS_VERSION=${JBOSS_VERSION} -Dquarkus-project-discovery=false -Dquarkus-cp-cache=false -Dquarkus-workspace-discovery=false -Dquarkus-classpath-cache=false
 sed -i 's/<version>'"${QUARKUS_VERSION}"'<\/version>/<version>${QUARKUS_VERSION}<\/version>/g' ./modules/testcases/jdkAll/Protean/quarkus/quark/test-configurations/pom.xml
 
