ded18961a90e5cd2514a220257baf493e2bd03fe
Panagiotis Sotiropoulos <psotirop@redhat.com>
2019-07-09 13:53:20 +0200

    Update MiniAdmin.java

--- modules/src2/main/java/com/mysql/cj/jdbc/admin/MiniAdmin.java
+++ modules/src2/main/java/com/mysql/cj/jdbc/admin/MiniAdmin.java
@@ -2,5 +2,5 @@ package com.mysql.cj.jdbc.admin;
 
 import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
 
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/security/src/main/java#17.0.0.Final","modules/testcases/jdkAll/Eap72x-Proposed/security/src/main/java#7.2.2","modules/testcases/jdkAll/Eap72x/security/src/main/java#7.2.2","modules/testcases/jdkAll/Eap7/security/src/main/java#7.3.0.CD17"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/security/src/main/java#17.0.0.Final","modules/testcases/jdkAll/Eap72x-Proposed/security/src/main/java#7.2.4","modules/testcases/jdkAll/Eap72x/security/src/main/java#7.2.4","modules/testcases/jdkAll/Eap7/security/src/main/java#7.3.0.CD17"})
 public class MiniAdmin{}
