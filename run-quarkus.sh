291bb5d342756fcbab8ba741898afe1014a95002
Panagiotis Sotiropoulos <psotirop@redhat.com>
2019-07-10 10:55:56 +0200

    Update run-quarkus.sh

--- run-quarkus.sh
+++ run-quarkus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-readonly NATIVE=${NATIVE:-''} # export NATIVE=-Pnative
+readonly NATIVE=${NATIVE:-'-Dnon-native'} # export NATIVE=-Pnative
 
 sed -i 's/${QUARKUS_VERSION}/'"${QUARKUS_VERSION}"'/g' ./modules/testcases/jdkAll/Protean/quarkus/quark/test-configurations/pom.xml
 ./Maven/apache-maven-3.5.3/bin/mvn clean install -Dquarkus "${NATIVE}" -DJBOSS_VERSION=${JBOSS_VERSION} -Dquarkus-project-discovery=false -Dquarkus-cp-cache=false -Dquarkus-workspace-discovery=false -Dquarkus-classpath-cache=false
