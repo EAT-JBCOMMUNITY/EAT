1c7d5170838edf43a8f7ea6eff140807cf9a2dfa
panossot <psotirop@redhat.com>
2019-08-01 08:50:08 +0200

    Testing DriverManagerConnectionSource exclusion ...
    Update SecurityDeserializationTestCase.java

---
+++ modules/src/main/java/ch/qos/logback/core/db/DriverManagerConnectionSource.java
@@ -0,0 +1,8 @@
+package ch.qos.logback.core.db;
+
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/security/src/main/java#17.0.0.Final","modules/testcases/jdkAll/Eap72x-Proposed/security/src/main/java#7.2.4","modules/testcases/jdkAll/Eap72x/security/src/main/java#7.2.4"})
+public class DriverManagerConnectionSource {
+
+}
