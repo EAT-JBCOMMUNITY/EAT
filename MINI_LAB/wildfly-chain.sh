2f30393591a27e71ae47fc3207b3a0b6fc9b5d77
panossot <psotirop@redhat.com>
2019-09-02 10:57:00 +0200

    How component chains can be used with MINI LAB and Wildfly ...

---
+++ MINI_LAB/wildfly-chain.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd ./Dockerfiles/wildfly-chain
+docker build -t docker.io/eat . > outputWildflyChain.txt
+
+#sudo docker stop $(sudo docker ps -a -q)
+#sudo docker rm $(sudo docker ps -a -q)
+sudo docker rmi -f docker.io/eat
+
+
