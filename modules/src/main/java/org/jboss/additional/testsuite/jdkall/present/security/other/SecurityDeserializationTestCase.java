3489f3abb202f9d0d6d5716f8ccad213e9ef8795
panossot <psotirop@redhat.com>
2019-11-22 12:06:58 +0100

    Adding a testcase for multiple versions ...

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/security/other/SecurityDeserializationTestCase.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/security/other/SecurityDeserializationTestCase.java
@@ -271,6 +271,25 @@ public class SecurityDeserializationTestCase {
 
     }
 
+    @ATTest({"modules/testcases/jdkAll/Wildfly/security/src/main/java#19.0.0.Final","modules/testcases/jdkAll/Eap72x-Proposed/security/src/main/java#7.2.7","modules/testcases/jdkAll/Eap72x/security/src/main/java#7.2.7","modules/testcases/jdkAll/Eap7/security/src/main/java#7.3.1"})
+    public void testSecuirtyDatabind12() throws Exception {
+
+        final String JSON = aposToQuotes(
+                "{'v':['org.apache.log4j.receivers.db.DriverManagerConnectionSource']}"
+                 );
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enableDefaultTyping();
+
+        try {
+            PolyWrapper sc = mapper.readValue(JSON, PolyWrapper.class);
+            fail("Should not be able to deserialize because of security prevention.");
+        }catch(JsonMappingException e){
+            assertTrue("Fail because of security issues...",e.getMessage().contains("prevented for security reasons"));
+        }
+
+    }
+
     static class SerializedClass {
 
         public int id;
