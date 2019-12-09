072380349614fe108710ebcde2930c18ee6b267e
panossot <psotirop@redhat.com>
2019-12-09 15:00:00 +0100

    Adding a multiverion testcase for ejb exception handling ...

---
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/exceptions/MyApplicationExceptionNoRollbackRuntimeException.java
@@ -0,0 +1,39 @@
+package org.jboss.additional.testsuite.jdkall.present.ejb.exceptions;
+
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+import javax.ejb.ApplicationException;
+
+/**
+ * @author bmaxwell
+ *
+ */
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/ejb/src/main/java#19.0.0.Beta1", "modules/testcases/jdkAll/Eap72x/ejb/src/main/java#7.2.7", "modules/testcases/jdkAll/Eap72x-Proposed/ejb/main/java#7.2.7","modules/testcases/jdkAll/Eap7/ejb/src/main/java#7.3.1"})
+public class MyApplicationExceptionNoRollbackRuntimeException extends RuntimeException {
+    /**
+     *
+     */
+    public MyApplicationExceptionNoRollbackRuntimeException() {
+    }
+
+    /**
+     * @param message
+     */
+    public MyApplicationExceptionNoRollbackRuntimeException(String message) {
+        super(message);
+    }
+
+    /**
+     * @param cause
+     */
+    public MyApplicationExceptionNoRollbackRuntimeException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public MyApplicationExceptionNoRollbackRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
