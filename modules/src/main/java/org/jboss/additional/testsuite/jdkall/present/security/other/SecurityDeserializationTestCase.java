538e33499efc559e203407a61e16427001e48ada
panossot <psotirop@redhat.com>
2019-12-09 10:46:35 +0100

    Adding a multiverion testcase ...

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/security/other/SecurityDeserializationTestCase.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/security/other/SecurityDeserializationTestCase.java
@@ -345,6 +345,24 @@ public class SecurityDeserializationTestCase {
         }
     }
 
+    @ATTest({"modules/testcases/jdkAll/Wildfly/security/src/main/java#19.0.0.Beta1","modules/testcases/jdkAll/Eap72x-Proposed/security/src/main/java#7.2.6","modules/testcases/jdkAll/Eap72x/security/src/main/java#7.2.6","modules/testcases/jdkAll/Eap7/security/src/main/java#7.3.1.GA"})
+    public void testSecuirtyDatabind16() throws Exception {
+
+        final String JSON = aposToQuotes(
+                "{'v':['org.apache.commons.dbcp.datasources.PerUserPoolDataSource']}"
+                 );
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enableDefaultTyping();
+
+        try {
+            PolyWrapper sc = mapper.readValue(JSON, PolyWrapper.class);
+            fail("Should not be able to deserialize because of security prevention.");
+        }catch(JsonMappingException e){
+            assertTrue("Fail because of security issues...",e.getMessage().contains("prevented for security reasons"));
+        }
+    }
+
     static class SerializedClass {
 
         public int id;
