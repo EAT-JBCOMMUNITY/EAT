013006ab6b81d09b030a97cb837319018b904160
panossot <psotirop@redhat.com>
2019-12-16 12:44:18 +0100

    Addition of test versioning for ClientExceptionTestCase

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/exceptions/Hello.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/exceptions/Hello.java
@@ -7,7 +7,7 @@ import javax.ejb.Local;
  * @author bmaxwell
  *
  */
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/ejb/src/main/java", "modules/testcases/jdkAll/Eap72x/ejb/src/main/java", "modules/testcases/jdkAll/Eap72x-Proposed/ejb/main/java","modules/testcases/jdkAll/Eap7/ejb/src/main/java"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/ejb/src/main/java#19.0.0.Beta1", "modules/testcases/jdkAll/Eap72x/ejb/src/main/java#7.2.7", "modules/testcases/jdkAll/Eap72x-Proposed/ejb/main/java#7.2.7","modules/testcases/jdkAll/Eap7/ejb/src/main/java#7.3.1"})
 @Local
 public interface Hello {
     public String hello();
