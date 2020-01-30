0ee98e7bfc979bd95c501d0ed0a2f0075eff9c1c
panossot <psotirop@redhat.com>
2020-01-30 13:39:08 +0100

    Another versioning update.

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/asynctransactions/AsyncTransactionTestCase.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/asynctransactions/AsyncTransactionTestCase.java
@@ -22,7 +22,7 @@ import org.junit.Assert;
 
 @RunWith(Arquillian.class)
 @ServerSetup(ServerLogSetupTask.class)
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Eap72x/ejb/src/main/java#7.2.7","modules/testcases/jdkAll/Eap72x-Proposed/ejb/src/main/java#7.2.7","modules/testcases/jdkAll/Eap7/ejb/src/main/java#7.3.1","modules/testcases/jdkAll/Wildfly/ejb/src/main/java#19.0.0.Beta1"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Eap72x/ejb/src/main/java#7.2.7","modules/testcases/jdkAll/Eap72x-Proposed/ejb/src/main/java#7.2.7","modules/testcases/jdkAll/Eap7/ejb/src/main/java#7.3.0","modules/testcases/jdkAll/Wildfly/ejb/src/main/java#19.0.0.Beta1"})
 public class AsyncTransactionTestCase {
 
     @EJB
