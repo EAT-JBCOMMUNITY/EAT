3a375fe4b324a4abec9050d8ce932709ddb92976
Panagiotis Sotiropoulos <psotirop@redhat.com>
2020-03-30 09:30:12 +0200

    Addition of multiversion testcase regarding deserialization of XaPooledConnectionFactory and ResourceRef.

---
+++ modules/src2/main/java/com/caucho/config/types/ResourceRef.java
@@ -0,0 +1,7 @@
+package com.caucho.config.types;
+
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/jaxrs/src/main/java#20.0.0.Beta1","modules/testcases/jdkAll/Eap72x/jaxrs/src/main/java#7.2.9","modules/testcases/jdkAll/Eap72x-Proposed/jaxrs/src/main/java#7.2.9","modules/testcases/jdkAll/Eap7/jaxrs/src/main/java#7.3.2"})
+public class ResourceRef {
+}
