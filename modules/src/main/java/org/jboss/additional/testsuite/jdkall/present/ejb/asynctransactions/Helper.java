e03c225cc160d117333f7052ef0c2c1a288a0e48
Panagiotis Sotiropoulos <psotirop@redhat.com>
2020-04-07 18:02:57 +0200

    Versioning update ...

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/asynctransactions/Helper.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/asynctransactions/Helper.java
@@ -7,7 +7,7 @@ import javax.transaction.Transaction;
 import javax.transaction.TransactionManager;
 import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
 
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Eap72x/ejb/src/main/java#7.2.7","modules/testcases/jdkAll/Eap72x-Proposed/ejb/src/main/java#7.2.7","modules/testcases/jdkAll/Eap7/ejb/src/main/java#7.3.0","modules/testcases/jdkAll/Wildfly/ejb/src/main/java#19.0.0.Beta1"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Eap72x/ejb/src/main/java#7.2.7","modules/testcases/jdkAll/Eap72x-Proposed/ejb/src/main/java#7.2.7","modules/testcases/jdkAll/Eap7/ejb/src/main/java#7.3.0.GA","modules/testcases/jdkAll/Wildfly/ejb/src/main/java#19.0.0.Beta1"})
 public class Helper {
 
     static Transaction getCurrentTransaction() {
--- modules/src2/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/asynctransactions/AsyncTransaction.java
+++ modules/src2/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/asynctransactions/AsyncTransaction.java
@@ -10,7 +10,7 @@ import javax.ejb.TransactionManagementType;
 import javax.ejb.Stateless;
 import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
 
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Eap72x/ejb/src/main/java#7.2.7","modules/testcases/jdkAll/Eap72x-Proposed/ejb/src/main/java#7.2.7","modules/testcases/jdkAll/Eap7/ejb/src/main/java#7.3.0","modules/testcases/jdkAll/Wildfly/ejb/src/main/java#19.0.0.Beta1"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Eap72x/ejb/src/main/java#7.2.7","modules/testcases/jdkAll/Eap72x-Proposed/ejb/src/main/java#7.2.7","modules/testcases/jdkAll/Eap7/ejb/src/main/java#7.3.0.GA","modules/testcases/jdkAll/Wildfly/ejb/src/main/java#19.0.0.Beta1"})
 @Stateless
 @TransactionManagement(TransactionManagementType.CONTAINER)
 @TransactionAttribute(TransactionAttributeType.REQUIRED)
