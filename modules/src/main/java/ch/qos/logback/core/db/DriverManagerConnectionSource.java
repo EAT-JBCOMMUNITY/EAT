17b13081860f75b9628e850bd2ee890dcfc79344
Panagiotis Sotiropoulos <psotirop@redhat.com>
2020-04-07 18:39:10 +0200

    Adding a test ...

--- modules/src/main/java/ch/qos/logback/core/db/DriverManagerConnectionSource.java
+++ modules/src/main/java/ch/qos/logback/core/db/DriverManagerConnectionSource.java
@@ -2,7 +2,7 @@ package ch.qos.logback.core.db;
 
 import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
 
-@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/security/src/main/java#17.0.0.Final","modules/testcases/jdkAll/Eap72x-Proposed/security/src/main/java#7.2.4","modules/testcases/jdkAll/Eap72x/security/src/main/java#7.2.4"})
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/Wildfly/security/src/main/java#17.0.0.Final","modules/testcases/jdkAll/Eap72x-Proposed/security/src/main/java#7.2.4","modules/testcases/jdkAll/Eap72x/security/src/main/java#7.2.4","modules/testcases/jdkAll/Eap7/security/src/main/java#7.3.0.GA"})
 public class DriverManagerConnectionSource {
 
 }
