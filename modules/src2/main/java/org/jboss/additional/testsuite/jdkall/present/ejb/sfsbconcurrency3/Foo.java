f759a2510d660ff95b29a11380790b59fa791dcd
Panagiotis Sotiropoulos <panossot@gmail.com>
2020-06-09 20:16:09 +0200

    Adding some testcases ...

---
+++ modules/src2/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/sfsbconcurrency3/Foo.java
@@ -0,0 +1,9 @@
+package org.jboss.additional.testsuite.jdkall.present.ejb.sfsbconcurrency3;
+import org.jboss.eap.additional.testsuite.annotations.EAT;
+
+@EAT({"modules/testcases/jdkAll/Wildfly/ejb/src/main/java","modules/testcases/jdkAll/Eap72x/ejb/src/main/java","modules/testcases/jdkAll/Eap72x-Proposed/ejb/src/main/java","modules/testcases/jdkAll/Eap7/ejb/src/main/java"})
+public class Foo {
+    public void foo() {
+           
+	}
+}
