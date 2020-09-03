665f9bedd2605ebe63e6ace10c510828d77300f4
Panagiotis Sotiropoulos <panossoto@gmail.com>
2020-09-03 20:48:32 +0200

    Adding activemqAT at the ci menu ...

--- CI/run.sh
+++ CI/run.sh
@@ -20,6 +20,25 @@ elif [ "$1" == "-all" ]; then
 	./all.sh $2 $3
 elif [ "$1" == "-at" ]; then
 	./at.sh
+elif [ "$1" == "-activemq" ]; then
+        mkdir activemqdir
+        cd activemqdir
+	export PROGRAM="https://github.com/apache/activemq"
+	export AT="https://github.com/panossot/ActivemqAT"
+	wget https://raw.githubusercontent.com/apache/activemq/master/pom.xml
+	version=$(grep -m2 '<version>' ./pom.xml | tail -n1);
+	version=$(echo $version | grep -Po '>.*<\/');
+        version=$(echo "${version:1:${#version}}");
+        version=$(echo "${version:0:${#version}-2}");
+	export ACTIVEMQ_BRANCH_VERSION=$version
+	export TEST_CATEGORY=master
+	
+	echo ""
+	
+	echo "Building: Latest Apache ActiveMQ + ActiveMQ AT"
+	echo ""
+	cd ..
+	./pr.sh
 else 
 	./pr.sh
 fi
