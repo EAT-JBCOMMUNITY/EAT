21d39807259b92d174886aab0c999f2fcc634966
Panagiotis Sotiropoulos <panossoto@gmail.com>
2020-10-02 23:43:57 +0200

    Adding clear command

--- CIUI/CIUI/src/ciui/Commands.java
+++ CIUI/CIUI/src/ciui/Commands.java
@@ -6,6 +6,7 @@ public enum Commands {
     ACTIVEMQ_ARTEMIS("-activemq-artemis"),
     ALL("-all"),
     AT("-at"),
+    CLEAR("-clear"),
     JBOSS_MODULES("-jboss-modules"),
     JBOSS_THREADS("-jboss-threads"),
     OPENLIBERTY("-openliberty"),
