ec9dff96d3fddfaeead9c6e600acca6ccab1a447
Panagiotis Sotiropoulos <panossoto@gmail.com>
2020-10-10 20:27:01 +0200

    Adding auto scroll for display output in ci ui

--- CIUI/CIUI/src/ciui/PanelMain.java
+++ CIUI/CIUI/src/ciui/PanelMain.java
@@ -25,10 +25,11 @@ import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.text.DefaultCaret;
 
 
 public class PanelMain extends JPanel{
-    
+   
     private final int FIELD_SIZE=15;
     private final String[] options = {"Specific Pull Request", "All Pull Requests", "-activemq", "-activemq-artemis", "-clear", "-jboss-modules", "-jboss-threads", "-openliberty", "-springboot", "-wildfly"};
     private JLabel msg_label, fixed_command_label;
@@ -38,10 +39,10 @@ public class PanelMain extends JPanel{
     private JTextField program_all_field, at_all_field;
     private JTextArea output_log;
     private JComboBox program_build_combo;
-    
+   
     public PanelMain(){
         setLayout(new FlowLayout(FlowLayout.LEADING, 10, 10));
-        
+       
         JPanel left_panel = new JPanel();
         left_panel.setLayout(new FlowLayout(FlowLayout.LEADING, 10, 20));
         DefaultListModel list_model = new DefaultListModel<>();
@@ -53,14 +54,14 @@ public class PanelMain extends JPanel{
         list.setVisibleRowCount(15);
         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         list.setSelectedIndex(0);
-        
+       
         JScrollPane list_scrollpane = new JScrollPane(list);
         left_panel.add(list_scrollpane);
         add(left_panel);
-        
+       
         JPanel middle_panel = new JPanel();
         middle_panel.setLayout(new BoxLayout(middle_panel, BoxLayout.Y_AXIS));
-        
+       
         JPanel label_panel = new JPanel();
         label_panel.setLayout(new BorderLayout());
         msg_label = new JLabel();
@@ -71,10 +72,10 @@ public class PanelMain extends JPanel{
         JPanel inputs = new JPanel();
         CardLayout cl = new CardLayout();
         inputs.setLayout(cl);
-        
+       
         JPanel inputs_pr = new JPanel();
         inputs_pr.setLayout(new BoxLayout(inputs_pr, BoxLayout.Y_AXIS));
-      
+     
         JPanel group_1 = new JPanel();
         group_1.setLayout(new BorderLayout());
         group_1.add(new JLabel("PROGRAM"), BorderLayout.WEST);
@@ -90,7 +91,7 @@ public class PanelMain extends JPanel{
         group_2.add(program_pr_field, BorderLayout.EAST);
         inputs_pr.add(group_2);
         inputs_pr.add(Box.createRigidArea(new Dimension(0, 5)));
-        
+       
         JPanel group_3 = new JPanel();
         group_3.setLayout(new BorderLayout());
         group_3.add(new JLabel("PROGRAM_BRANCH "), BorderLayout.WEST);
@@ -98,7 +99,7 @@ public class PanelMain extends JPanel{
         group_3.add(program_branch_field, BorderLayout.EAST);
         inputs_pr.add(group_3);
         inputs_pr.add(Box.createRigidArea(new Dimension(0, 5)));
-        
+       
         JPanel group_4 = new JPanel();
         group_4.setLayout(new BorderLayout());
         group_4.add(new JLabel("AT"), BorderLayout.WEST);
@@ -106,7 +107,7 @@ public class PanelMain extends JPanel{
         group_4.add(at_field, BorderLayout.EAST);
         inputs_pr.add(group_4);
         inputs_pr.add(Box.createRigidArea(new Dimension(0, 5)));
-        
+       
         JPanel group_5 = new JPanel();
         group_5.setLayout(new BorderLayout());
         group_5.add(new JLabel("AT_PR"), BorderLayout.WEST);
@@ -114,7 +115,7 @@ public class PanelMain extends JPanel{
         group_5.add(at_pr_field, BorderLayout.EAST);
         inputs_pr.add(group_5);
         inputs_pr.add(Box.createRigidArea(new Dimension(0, 5)));
-        
+       
         JPanel group_6 = new JPanel();
         group_6.setLayout(new BorderLayout());
         group_6.add(new JLabel("AT_BRANCH"), BorderLayout.WEST);
@@ -122,7 +123,7 @@ public class PanelMain extends JPanel{
         group_6.add(at_branch_field, BorderLayout.EAST);
         inputs_pr.add(group_6);
         inputs_pr.add(Box.createRigidArea(new Dimension(0, 5)));
-        
+       
         JPanel group_7 = new JPanel();
         group_7.setLayout(new BorderLayout());
         group_7.add(new JLabel("TEST_CATEGORY"), BorderLayout.WEST);
@@ -130,7 +131,7 @@ public class PanelMain extends JPanel{
         group_7.add(at_test_category_field, BorderLayout.EAST);
         inputs_pr.add(group_7);
         inputs_pr.add(Box.createRigidArea(new Dimension(0, 5)));
-        
+       
         JPanel group_8 = new JPanel();
         group_8.setLayout(new BorderLayout());
         group_8.add(new JLabel("PROGRAM_BUILD"), BorderLayout.WEST);
@@ -153,7 +154,7 @@ public class PanelMain extends JPanel{
         group_9.add(program_all_field, BorderLayout.EAST);
         inputs_all.add(group_9);
         inputs_all.add(Box.createRigidArea(new Dimension(0, 5)));
-        
+       
         JPanel group_10 = new JPanel();
         group_10.setLayout(new BorderLayout());
         group_10.add(new JLabel("AT"), BorderLayout.WEST);
@@ -161,22 +162,22 @@ public class PanelMain extends JPanel{
         group_10.add(at_all_field, BorderLayout.EAST);
         inputs_all.add(group_10);
         inputs_all.add(Box.createRigidArea(new Dimension(0, 5)));
-        
+       
         inputs_all.add(Box.createRigidArea(new Dimension(0, group_1.getPreferredSize().height*8)));
         inputs.add(inputs_all, "all");
-        
+       
         JPanel inputs_fixed = new JPanel();
         inputs_fixed.setLayout(new BoxLayout(inputs_fixed, BoxLayout.Y_AXIS));
         fixed_command_label = new JLabel();
         inputs_fixed.add(fixed_command_label);
-        
+       
         inputs.add(inputs_fixed, "fixed");
         //Show PR card
         cl.show(inputs, "pr");
-        
+       
         middle_panel.add(inputs);
         middle_panel.add(Box.createRigidArea(new Dimension(0, 32)));
-        
+       
         add(middle_panel);
 
         start = new JButton("Start");
@@ -184,20 +185,23 @@ public class PanelMain extends JPanel{
         start.setMaximumSize(new Dimension(300, 30));
         start.setAlignmentX(Component.CENTER_ALIGNMENT);
         middle_panel.add(start);
-        
+       
         JPanel right_panel = new JPanel();
         right_panel.setLayout(new BoxLayout(right_panel, BoxLayout.Y_AXIS));
         right_panel.setPreferredSize(new Dimension(300, 300));
-            
+           
         output_log = new JTextArea(5, 10);
         output_log.setLineWrap(true);
         output_log.setEditable(false);
+        JTextArea textArea = new JTextArea();
+        DefaultCaret caret = (DefaultCaret)output_log.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
         JScrollPane scroll = new JScrollPane(output_log);
         scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         right_panel.add(scroll);
 
         add(right_panel);
-        
+       
         //List listener
         list.addListSelectionListener(new ListSelectionListener() {
             @Override
@@ -219,14 +223,14 @@ public class PanelMain extends JPanel{
             }
         });
     }
-    
+   
     public void addActionListener(ActionListener lis) {
         start.addActionListener(lis);
     }
-    
+   
     public Map getParameters() {
         Map<String, String> map = new HashMap<>();
-        
+       
         switch ((String)list.getSelectedValue()) {
             case "Specific Pull Request":
                 map.put("PROGRAM", program_field.getText());
@@ -244,13 +248,13 @@ public class PanelMain extends JPanel{
                 break;
             default:
         }
-        
+       
         return map;
     }
-    
+   
     public Commands getCommand() {
         Commands command = null;
-        
+       
         switch ((String)list.getSelectedValue()) {
             case "Specific Pull Request":
                 command = Commands.AT;
@@ -288,22 +292,23 @@ public class PanelMain extends JPanel{
        
         return command;
     }
-    
+   
     public void successMessage(String text) {
         msg_label.setForeground(new Color(13,169,13));
         msg_label.setText(text);
     }
-    
+   
     public void failureMessage(String text) {
         msg_label.setForeground(Color.RED);
         msg_label.setText(text);
     }
-    
+   
     public void appendOutputLog(String text) {
         output_log.setText(output_log.getText()+text);
     }
-    
+   
     public void clearOutputLog() {
         output_log.setText("");
     }
 }
+
