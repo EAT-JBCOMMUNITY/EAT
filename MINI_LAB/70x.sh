e71bd345a0ec624e3c6d448891310f51cae1a8b6
Panagiotis Sotiropoulos <panossoto@gmail.com>
2020-10-11 03:27:23 +0200

    Update 70x.sh

--- MINI_LAB/70x.sh
+++ MINI_LAB/70x.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd ./Dockerfiles/JBossServers70x
-docker build -t docker.io/eat70x . > outputEap70x.txt
+docker build -t docker.io/eat70x --ulimit nofile=5000:5000 . > outputEap70x.txt
 
 #sudo docker stop $(sudo docker ps -a -q)
 #sudo docker rm $(sudo docker ps -a -q)
