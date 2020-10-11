05c65088650f965a4befdadd924900b30dec55b3
Panagiotis Sotiropoulos <panossoto@gmail.com>
2020-10-11 03:28:54 +0200

    Update 72x.sh

--- MINI_LAB/72x.sh
+++ MINI_LAB/72x.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd ./Dockerfiles/JBossServers72x
-docker build -t docker.io/eat72x . > outputEap72x.txt
+docker build -t docker.io/eat72x --ulimit nofile=5000:5000 . > outputEap72x.txt
 
 #sudo docker stop $(sudo docker ps -a -q)
 #sudo docker rm $(sudo docker ps -a -q)
