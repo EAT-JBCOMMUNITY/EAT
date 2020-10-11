cba3229220cd15c6754256f029eba7db2686c61d
Panagiotis Sotiropoulos <panossoto@gmail.com>
2020-10-11 03:27:51 +0200

    Update 71x.sh

--- MINI_LAB/71x.sh
+++ MINI_LAB/71x.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd ./Dockerfiles/JBossServers71x
-docker build -t docker.io/eat71x . > outputEap71x.txt
+docker build -t docker.io/eat71x --ulimit nofile=5000:5000 . > outputEap71x.txt
 
 #sudo docker stop $(sudo docker ps -a -q)
 #sudo docker rm $(sudo docker ps -a -q)
