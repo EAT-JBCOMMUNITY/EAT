1600408844c4f70c16ae3344ad358b47234156b2
Panagiotis Sotiropoulos <panossoto@gmail.com>
2020-11-29 17:27:17 +0100

    Update daemon.sh

--- CI/daemon.sh
+++ CI/daemon.sh
@@ -41,7 +41,7 @@ do      #Additional filters could be added here (e.g. pr,version filters for run
        uts_ckecked=$(echo ${checked_eat_prs_uts[$k]}  | tr -cd [:digit:]);
        if [ "$uts" -gt "$uts_ckecked" ]; then
            echo $(date) ... Updated pr : $pr_num
-       #    docker run --rm --name atci_$pr_num -e TEST_PROGRAM=wildfly -e AT_PR=$pr_num -e GITHUB_TOKEN=$GITHUB_TOKEN -v $HOME/.m2/repository:/home/user/.m2/repository --privileged=true --ulimit nofile=5000:5000 docker.io/atci > output_$pr_num.txt &
+           docker run --rm --name atci_$pr_num -e TEST_PROGRAM=wildfly -e AT_PR=$pr_num -e GITHUB_TOKEN=$GITHUB_TOKEN -v $HOME/.m2/repository:/home/user/.m2/repository --privileged=true --ulimit nofile=5000:5000 docker.io/atci > output_$pr_num.txt &
        fi
        k=$((k+1));
        c=$((c+1));
