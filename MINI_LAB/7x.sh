87d013b2dd66ec4a39ec7c4ef39056776b029e16
Panagiotis Sotiropoulos <psotirop@redhat.com>
2021-02-14 00:16:44 +0100

    Adding mini lab for 7x in general (for customizations)...

---
+++ MINI_LAB/7x.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd ./Dockerfiles/JBossServers7x
+docker build -t docker.io/eat7x --ulimit nofile=5000:5000 . > outputEap7x.txt
+
+#sudo docker stop $(sudo docker ps -a -q)
+#sudo docker rm $(sudo docker ps -a -q)
+sudo docker rmi -f docker.io/eat7x
