000e8a167ca9d51f5fba3d6ed4d4db3c29abcece
Panagiotis Sotiropoulos <panossoto@gmail.com>
2021-02-14 09:02:53 +0100

    Adding a spring test to eap7 server ...

--- modules/src/main/java/org/springboot/JaxbResource.java
+++ modules/src/main/java/org/springboot/JaxbResource.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.jboss.eap.additional.testsuite.annotations.EAT;
 
 @RestController
-@EAT({"modules/testcases/jdkAll/Wildfly/spring/src/main/java#23.0.0"})
+@EAT({"modules/testcases/jdkAll/Wildfly/spring/src/main/java#23.0.0","modules/testcases/jdkAll/Eap7/spring/src/main/java#7.3.5"})
 public class JaxbResource {
     @RequestMapping("/restspring/{resource}")
     String Resource(@PathVariable String resource) {
