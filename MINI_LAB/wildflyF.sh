c39287c7ad6c146e95702d1d18329a15abfa0699
Panagiotis Sotiropoulos <panossoto@gmail.com>
2021-03-07 04:55:56 +0100

    Update wildflyF.sh

--- MINI_LAB/wildflyF.sh
+++ MINI_LAB/wildflyF.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 cd ./Dockerfiles/wildfly
-docker build -t docker.io/eat -f DockerfileFedora --ulimit nofile=5000:5000 . > outputWildflyF.txt
+docker build -t docker.io/eatF -f DockerfileFedora --ulimit nofile=5000:5000 . > outputWildflyF.txt
 
 #sudo docker stop $(sudo docker ps -a -q)
 #sudo docker rm $(sudo docker ps -a -q)
-sudo docker rmi -f docker.io/eat
+sudo docker rmi -f docker.io/eatF
 
 
