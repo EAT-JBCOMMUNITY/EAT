75b91003499102f168f7257fedd6dc88ec2e4ddd
Panagiotis Sotiropoulos <panossoto@gmail.com>
2021-03-07 04:56:42 +0100

    Update quarkusF.sh

--- MINI_LAB/quarkusF.sh
+++ MINI_LAB/quarkusF.sh
@@ -2,7 +2,7 @@
 
 
 cd ./Dockerfiles/quarkus
-docker build -t docker.io/eat -f DockerfileFedora --ulimit nofile=5000:5000 . > outputQuarkus.txt
+docker build -t docker.io/eat -f DockerfileFedora --ulimit nofile=5000:5000 . > outputQuarkusF.txt
 
 sudo docker stop $(sudo docker ps -a -q)
 sudo docker rm $(sudo docker ps -a -q)
