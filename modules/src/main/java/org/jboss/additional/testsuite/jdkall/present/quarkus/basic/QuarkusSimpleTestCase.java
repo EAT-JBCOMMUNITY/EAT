6299d1151b46b67d0cecd4c6f3add0524588f752
Panagiotis Sotiropoulos <panossoto@gmail.com>
2021-03-13 21:35:17 +0100

    Distributing test source to new category ...

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/quarkus/basic/QuarkusSimpleTestCase.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/quarkus/basic/QuarkusSimpleTestCase.java
@@ -29,7 +29,7 @@ import org.junit.jupiter.api.Test;
 import io.quarkus.test.junit.QuarkusTest;
 
 
-@EAT({"modules/testcases/jdkAll/Protean/quarkus/protean/test-configurations2/src/test/java#0.10.0*0.12.0","modules/testcases/jdkAll/Protean/quarkus/quark/test-configurations/src/test/java#0.13.0*999.0.1"})
+@EAT({"modules/testcases/jdkAll/Protean/quarkus/protean/test-configurations2/src/test/java#0.10.0*0.12.0","modules/testcases/jdkAll/Protean/quarkus/quark/test-configurations/src/test/java#0.13.0*999.0.1","modules/testcases/jdkAll/Protean/quarkus/quark2/test-configurations/src/main/java#999.0.0"})
 @QuarkusTest
 public class QuarkusSimpleTestCase {
 
