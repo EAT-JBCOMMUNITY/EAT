e0b943d0731aa262230d123c9b395486a6572fc2
Panagiotis Sotiropoulos <panossoto@gmail.com>
2021-10-13 22:48:10 +0200

    Update wildfly-chain.sh

--- MINI_LAB/wildfly-chain.sh
+++ MINI_LAB/wildfly-chain.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 cd ./Dockerfiles/wildfly-chain
-docker build -t docker.io/eat --ulimit nofile=5000:5000 . > outputWildflyChain.txt
+podman build -t docker.io/eat --ulimit nofile=5000:5000 . > outputWildflyChain.txt
 
-#sudo docker stop $(sudo docker ps -a -q)
-#sudo docker rm $(sudo docker ps -a -q)
-sudo docker rmi -f docker.io/eat
+#sudo podman stop $(sudo docker ps -a -q)
+#sudo podman rm $(sudo docker ps -a -q)
+sudo podman rmi -f docker.io/eat
 
 
