0fd7553a691109b10b87d76bfecf9adc2c3a0d82
Panagiotis Sotiropoulos <panossoto@gmail.com>
2021-10-16 20:41:00 +0200

    Update 73x.sh

--- MINI_LAB/73x.sh
+++ MINI_LAB/73x.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd ./Dockerfiles/JBossServers73x
-docker build -t docker.io/eat73x --ulimit nofile=5000:5000 . > outputEap73x.txt
+podman build -t docker.io/eat73x --ulimit nofile=5000:5000 . > outputEap73x.txt
 
-#sudo docker stop $(sudo docker ps -a -q)
-#sudo docker rm $(sudo docker ps -a -q)
-sudo docker rmi -f docker.io/eat73x
+#sudo podman stop $(sudo docker ps -a -q)
+#sudo podman rm $(sudo docker ps -a -q)
+sudo podman rmi -f docker.io/eat73x
