193b26a13044cdfa3387c9e3db9aa267e50683b0
Panagiotis Sotiropoulos <panossoto@gmail.com>
2021-10-16 20:40:24 +0200

    Update 7xF.sh

--- MINI_LAB/7xF.sh
+++ MINI_LAB/7xF.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd ./Dockerfiles/JBossServers7x
-docker build -t docker.io/eat7x -f DockerfileFedora --ulimit nofile=5000:5000 . > outputEap7x.txt
+podman build -t docker.io/eat7x -f DockerfileFedora --ulimit nofile=5000:5000 . > outputEap7x.txt
 
-#sudo docker stop $(sudo docker ps -a -q)
-#sudo docker rm $(sudo docker ps -a -q)
-sudo docker rmi -f docker.io/eat7x
+#sudo podman stop $(sudo docker ps -a -q)
+#sudo podman rm $(sudo docker ps -a -q)
+sudo podman rmi -f docker.io/eat7x
