80c9e11276f03ef1ac66a31e4a827820687e39b0
Panagiotis Sotiropoulos <panossoto@gmail.com>
2021-10-17 21:03:59 +0200

    Update 72x-chain.sh

--- MINI_LAB/72x-chain.sh
+++ MINI_LAB/72x-chain.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd ./Dockerfiles/JBossServers72x-chain
-docker build -t docker.io/eat72x --ulimit nofile=5000:5000 . > outputEap72xChain.txt
+podman build -t docker.io/eat72x --ulimit nofile=5000:5000 . > outputEap72xChain.txt
 
-#sudo docker stop $(sudo docker ps -a -q)
-#sudo docker rm $(sudo docker ps -a -q)
-sudo docker rmi -f docker.io/eat72x
+#sudo podman stop $(sudo docker ps -a -q)
+#sudo podman rm $(sudo docker ps -a -q)
+sudo podman rmi -f docker.io/eat72x
