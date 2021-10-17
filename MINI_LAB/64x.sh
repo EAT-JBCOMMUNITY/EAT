09dae784b868f56a6ef17ce567070bc6c6caa6e8
Panagiotis Sotiropoulos <panossoto@gmail.com>
2021-10-17 21:05:44 +0200

    Update 64x.sh

--- MINI_LAB/64x.sh
+++ MINI_LAB/64x.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 cd ./Dockerfiles/JBossServers64x
-docker build -t docker.io/eat64x --ulimit nofile=5000:5000 . > outputEap64x.txt
+podman build -t docker.io/eat64x --ulimit nofile=5000:5000 . > outputEap64x.txt
 
-#sudo docker stop $(sudo docker ps -a -q)
-#sudo docker rm $(sudo docker ps -a -q)
-sudo docker rmi -f docker.io/eat64x
+#sudo podman stop $(sudo docker ps -a -q)
+#sudo podman rm $(sudo docker ps -a -q)
+sudo podman rmi -f docker.io/eat64x
 
 
 
