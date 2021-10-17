83285e7ad14aa2be9965bc8f3673cc463830c04e
Panagiotis Sotiropoulos <panossoto@gmail.com>
2021-10-17 21:04:25 +0200

    Update 71xF.sh

--- MINI_LAB/71xF.sh
+++ MINI_LAB/71xF.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 cd ./Dockerfiles/JBossServers71x
-docker build -t docker.io/eat71x  -f DockerfileFedora --ulimit nofile=5000:5000 . > outputEap71xF.txt
+podman build -t docker.io/eat71x  -f DockerfileFedora --ulimit nofile=5000:5000 . > outputEap71xF.txt
 
-#sudo docker stop $(sudo docker ps -a -q)
-#sudo docker rm $(sudo docker ps -a -q)
-sudo docker rmi -f docker.io/eat71x
+#sudo podman stop $(sudo docker ps -a -q)
+#sudo podman rm $(sudo docker ps -a -q)
+sudo podman rmi -f docker.io/eat71x
 
 
 
