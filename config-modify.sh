c27ba56b9dcaaaf65b3b4ce34cf6fc7673d5ec5b
Panagiotis Sotiropoulos <panossoto@gmail.com>
2021-11-21 20:40:42 +0100

    Update config-modify.sh

--- config-modify.sh
+++ config-modify.sh
@@ -12,3 +12,16 @@ fi
 
 cp ${SERVER_POM_CONFIG} ./modules/testcases/jdkAll/${SERVER_DIR_NAME}
 
+if [ -z "${SERVER_CONFIG_DIR_DEST}" ]; then
+  echo "No SERVER_CONFIG_DIR_DEST has been defined."
+  exit 3
+fi
+
+if [ -z "${SERVER_CONFIG_DIR_TOCOPY}" ]; then
+  echo "No SERVER_CONFIG_DIR_TOCOPY has been defined."
+  exit 3
+fi
+
+rm -f ${SERVER_CONFIG_DIR_DEST}
+
+cp ${SERVER_CONFIG_DIR_TOCOPY} ${SERVER_CONFIG_DIR_DEST}
