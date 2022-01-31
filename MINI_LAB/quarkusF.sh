15e20b31eba0a48e2c177404618e224bf5cbec83
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-01-31 17:39:47 +0100

    Update quarkusF.sh

--- MINI_LAB/quarkusF.sh
+++ MINI_LAB/quarkusF.sh
@@ -2,7 +2,7 @@
 
 
 cd ./Dockerfiles/quarkus
-podman build -t docker.io/eat -f DockerfileFedora --ulimit nofile=5000:5000 . > outputQuarkusF.txt
+podman build -t docker.io/eat -f DockerfileFedora --ulimit nofile=10000:10000 . > outputQuarkusF.txt
 
 sudo podman stop $(sudo docker ps -a -q)
 sudo podman rm $(sudo docker ps -a -q)
