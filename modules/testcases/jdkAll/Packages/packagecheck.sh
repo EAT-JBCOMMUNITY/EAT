72e2e71ae6077f7df7d95d66b6f4c4471dd7ea02
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-02-27 19:11:14 +0200

    Minor correction

--- modules/testcases/jdkAll/Packages/packagecheck.sh
+++ modules/testcases/jdkAll/Packages/packagecheck.sh
@@ -1,5 +1,5 @@
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/NetworkManager-1.18.8-1.el7.x86_64.rpm
 
-rpm -q --whatprovides NetworkManage
+rpm -q --whatprovides NetworkManager
 
 rpm -qpR NetworkManager-1.18.8-1.el7.x86_64.rpm
