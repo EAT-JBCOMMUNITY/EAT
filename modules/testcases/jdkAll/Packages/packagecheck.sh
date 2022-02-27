1c17d5a3463fe214f14c83dec645f6baff127ed2
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-02-27 19:09:17 +0200

    Basic script check ...

---
+++ modules/testcases/jdkAll/Packages/packagecheck.sh
@@ -0,0 +1,5 @@
+wget http://mirror.centos.org/centos/7/os/x86_64/Packages/NetworkManager-1.18.8-1.el7.x86_64.rpm
+
+rpm -q --whatprovides NetworkManage
+
+rpm -qpR NetworkManager-1.18.8-1.el7.x86_64.rpm
