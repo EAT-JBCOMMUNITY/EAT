59a28d53466144dd5f8e5b1d5f305d33d012114e
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-06-05 17:48:22 +0300

    Update clone.sh

--- modules/testcases/jdkAll/Android/androidproject1/clone.sh
+++ modules/testcases/jdkAll/Android/androidproject1/clone.sh
@@ -1,7 +1,1 @@
-# GITHUB_REP and SUBDIR environment variables are needed
-# e.g export GITHUB_REPO=https://github.com/panossot/RemoteTestRepo.git
-#     export SUBDIR=RemoteTestRepo/src/java/main/org
-
 git clone https://github.com/EAT-JBCOMMUNITY/AndroidProject.git
-
-
