07250b0643572c1a949a1da5923ce12fe069606f
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-06-05 17:41:22 +0300

    Modifying android multiversion clone build process ...

---
+++ modules/testcases/jdkAll/Android/androidproject1/clone.sh
@@ -0,0 +1,7 @@
+# GITHUB_REP and SUBDIR environment variables are needed
+# e.g export GITHUB_REPO=https://github.com/panossot/RemoteTestRepo.git
+#     export SUBDIR=RemoteTestRepo/src/java/main/org
+
+git clone https://github.com/EAT-JBCOMMUNITY/AndroidProject.git
+
+
