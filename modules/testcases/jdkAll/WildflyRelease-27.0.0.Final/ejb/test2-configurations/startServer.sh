49fcae8ac670deea67f370725fc8337b25f293b4
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-06-18 21:59:29 +0300

    Preparing 27.0.0.Final wildfly server snapshot ...

---
+++ modules/testcases/jdkAll/WildflyRelease-27.0.0.Final/ejb/test2-configurations/startServer.sh
@@ -0,0 +1,3 @@
+# @EAT({"modules/testcases/jdkAll/WildflyRelease-13.0.0.Final/ejb/test2-configurations#13.0.0","modules/testcases/jdkAll/Wildfly/ejb/test2-configurations#13.0.0","modules/testcases/jdkAll/Eap72x/ejb/test2-configurations","modules/testcases/jdkAll/Eap72x-Proposed/ejb/test2-configurations","modules/testcases/jdkAll/Eap71x/ejb/test2-configurations","modules/testcases/jdkAll/Eap71x-Proposed/ejb/test2-configurations","modules/testcases/jdkAll/Eap7/ejb/test2-configurations"}) 
+
+./../../../../../servers/wildfly/build/target/jbossas/bin/standalone.sh>output.txt
