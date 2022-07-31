da28ed54d1cc437814b53a9d1ec034b344efd10f
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-07-31 18:17:55 +0300

    Elytron is jakarta-fied ...

--- modules/src1/main/java/org/jboss/additional/testsuite/jdkall/present/elytron/batch/jakarta/IdentityBatchlet.java
+++ modules/src1/main/java/org/jboss/additional/testsuite/jdkall/present/elytron/batch/jakarta/IdentityBatchlet.java
@@ -22,20 +22,32 @@
 
 package org.jboss.additional.testsuite.jdkall.present.elytron.batch;
 
-import javax.ejb.LocalBean;
-import javax.ejb.Stateless;
+import jakarta.batch.api.Batchlet;
+import jakarta.inject.Named;
 
-import org.jboss.ejb3.annotation.SecurityDomain;
+import org.jboss.logging.Logger;
+import org.wildfly.security.auth.server.SecurityDomain;
 import org.jboss.eap.additional.testsuite.annotations.EAT;
 
 /**
- * This is here as just a hack to be able to set the security domain of a deployment.
- * See https://issues.jboss.org/browse/JBEAP-8702 discussion for more context.
  * @author Jan Martiska
  */
-@EAT({"modules/testcases/jdkAll/WildflyRelease-13.0.0.Final/elytron/src/main/java","modules/testcases/jdkAll/WildflyRelease-24.0.0.Final/elytron/src/main/java","modules/testcases/jdkAll/Wildfly/elytron/src/main/java","modules/testcases/jdkAll/WildflyJakarta/elytron/src/main/java#27.0.0.Alpha4","modules/testcases/jdkAll/ServerBeta/elytron/src/main/java","modules/testcases/jdkAll/WildflyRelease-17.0.0.Final/elytron/src/main/java","modules/testcases/jdkAll/WildflyRelease-20.0.0.Final/elytron/src/main/java","modules/testcases/jdkAll/Eap72x/elytron/src/main/java","modules/testcases/jdkAll/Eap72x-Proposed/elytron/src/main/java","modules/testcases/jdkAll/Eap7Plus/elytron/src/main/java","modules/testcases/jdkAll/Eap71x-Proposed/elytron/src/main/java","modules/testcases/jdkAll/Eap71x/elytron/src/main/java"})
-@Stateless
-@SecurityDomain("BatchDomain2")
-@LocalBean
-public class SecurityDomainSettingEJB {
+@EAT({"modules/testcases/jdkAll/WildflyJakarta/elytron/src/main/java#27.0.0.Alpha4"})
+@Named
+public class IdentityBatchlet implements Batchlet {
+
+    private Logger logger = Logger.getLogger(IdentityBatchlet.class);
+
+    @Override
+    public String process() throws Exception {
+        final String name = SecurityDomain.getCurrent().getCurrentSecurityIdentity().getPrincipal().getName();
+        logger.info("Batchlet running as username: " + name);
+        BatchSubsystemSecurityTestCase.identityWithinJob.complete(name);
+        return "OK";
+    }
+
+    @Override
+    public void stop() throws Exception {
+
+    }
 }
