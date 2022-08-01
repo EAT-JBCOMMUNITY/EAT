791509a0a3659dda39dba36ea1f004abb94b7d16
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-08-02 02:40:20 +0300

    Update MainActivity.java

--- modules/src1/main/java/org/example/jkslinks/version2/MainActivity.java
+++ modules/src1/main/java/org/example/jkslinks/version2/MainActivity.java
@@ -2,7 +2,7 @@ package com.fari.jkslinks;
 
 import android.content.Intent;
 import android.net.Uri;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity; 
 import android.os.Bundle;
 import android.view.View;
 import android.telephony.emergency.EmergencyNumber;
