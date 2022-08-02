fb951cca4f0fd6da0027792c900a23435813fb6b
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-08-03 01:19:47 +0300

    More jk additions ...

---
+++ modules/src1/main/java/org/jboss/additional/testsuite/jdkall/present/batch/itemreadertransaction/jakarta/ItemreaderReader.java
@@ -0,0 +1,69 @@
+package org.jboss.additional.testsuite.jdkall.present.batch.itemreadertransaction;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import jakarta.annotation.Resource;
+import jakarta.batch.api.BatchProperty;
+import jakarta.batch.api.chunk.ItemReader;
+import jakarta.batch.runtime.context.JobContext;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.transaction.TransactionSynchronizationRegistry;
+import org.jboss.eap.additional.testsuite.annotations.EAT;
+
+@Named
+@EAT({"modules/testcases/jdkAll/Wildfly/batch/src/main/java#27.0.0","modules/testcases/jdkAll/WildflyJakarta/batch/src/main/java#27.0.0.Alpha4", "modules/testcases/jdkAll/Eap7Plus/batch/src/main/java#7.4.5"})
+public class ItemreaderReader implements ItemReader {
+
+    @Inject
+    @BatchProperty
+    private int limit;
+
+    @Inject
+    private JobContext context;
+
+    @Inject
+    private JobContext jobContext;
+
+    private int count = 0;
+
+    @Resource
+    private TransactionSynchronizationRegistry reg;
+
+    @Inject
+    private StepScopedBean bean;
+
+    @Override
+    public void open(Serializable checkpoint) throws Exception {
+        if (limit == 0) {
+            limit = 10;
+        }
+        if (checkpoint != null) {
+            count = (Integer) checkpoint;
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("Reader close(" + jobContext.getBatchStatus() + ", "
+                + jobContext.getExitStatus() + "): Transaction ID: " + reg.getTransactionKey());
+        bean.close();
+        System.out.println("bean.close() called: Transaction ID: " + reg.getTransactionKey());
+    }
+
+    @Override
+    public Object readItem() throws Exception {
+        if (Boolean.valueOf(context.getProperties()
+                .getProperty("fail"))) {
+            throw new IOException("fail");
+        }
+        return count++ >= limit ? null : "an item " + count;
+    }
+
+    @Override
+    public Serializable checkpointInfo() throws Exception {
+        return count;
+    }
+
+}
---
+++ modules/src1/main/java/org/jboss/additional/testsuite/jdkall/present/batch/itemreadertransaction/jakarta/StepScopedBean.java
@@ -0,0 +1,23 @@
+package org.jboss.additional.testsuite.jdkall.present.batch.itemreadertransaction;
+
+import jakarta.annotation.Resource;
+import jakarta.transaction.TransactionSynchronizationRegistry;
+import jakarta.transaction.Transactional;
+import jakarta.transaction.Transactional.TxType;
+
+import org.jberet.cdi.StepScoped;
+import org.jboss.eap.additional.testsuite.annotations.EAT;
+
+@StepScoped
+@EAT({"modules/testcases/jdkAll/Wildfly/batch/src/main/java#27.0.0","modules/testcases/jdkAll/WildflyJakarta/batch/src/main/java#27.0.0.Alpha4","modules/testcases/jdkAll/Eap7Plus/batch/src/main/java#7.4.5"})
+@Transactional(TxType.MANDATORY)
+public class StepScopedBean
+{
+  @Resource
+  private TransactionSynchronizationRegistry reg;
+
+  public void close()
+  {
+    System.out.println("StepScopedBean.close() called; Transaction ID: " + reg.getTransactionKey());
+  }
+}
