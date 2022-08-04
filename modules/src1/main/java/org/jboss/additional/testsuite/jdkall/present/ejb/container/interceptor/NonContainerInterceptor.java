a459d6cce91e90d89bab5a7a1f2b68a869eba42b
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-08-04 15:20:37 +0300

    Adding a testcase for  WFLY-16567 (currently failing)  ...

---
+++ modules/src1/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/container/interceptor/NonContainerInterceptor.java
@@ -0,0 +1,57 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.additional.testsuite.jdkall.present.ejb.container.interceptor;
+
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.InvocationContext;
+import java.util.Map;
+
+import org.jboss.logging.Logger;
+import org.jboss.eap.additional.testsuite.annotations.EAT;
+/**
+ * Simple interceptor, which adds its classname in front of the result of {@link InvocationContext#proceed()}. Result of the
+ * proceed() stays untouched in case the {@link InvocationContext#getContextData()} contains classname of this interceptor under
+ * the {@link FlowTrackingBean#CONTEXT_DATA_KEY} key.
+ *
+ * @author Jaikiran Pai
+ */
+//@AT({"modules/testcases/jdkAll/Wildfly/ejb/src/main/java#27.0.0.Alpha2*27.0.0.Alpha3"})
+public class NonContainerInterceptor {
+
+    private static final Logger logger = Logger.getLogger(NonContainerInterceptor.class);
+
+    @AroundInvoke
+    public Object someMethod(InvocationContext invocationContext) throws Exception {
+        logger.trace("Invoked non-container interceptor!!!");
+        Map<String, Object> contextData = invocationContext.getContextData();
+        System.out.println("contextData = " + contextData);
+
+        Object returnObject = invocationContext.proceed();
+
+        contextData.remove("AnyKey");
+        System.out.println("Removed contextData = " + contextData);
+
+        return returnObject;
+
+    }
+}
---
+++ modules/src1/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/container/interceptor/SimpleEJBClientInterceptor.java
@@ -0,0 +1,62 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.additional.testsuite.jdkall.present.ejb.container.interceptor;
+
+import java.util.Map;
+
+import org.jboss.ejb.client.EJBClientInterceptor;
+import org.jboss.ejb.client.EJBClientInvocationContext;
+import org.jboss.eap.additional.testsuite.annotations.EAT;
+/**
+ * An EJB client side interceptor. The entries in a {@link Map} provided to the constructor are copied to the
+ * {@link EJBClientInvocationContext#getContextData()} in the {@link #handleInvocation(EJBClientInvocationContext)} before
+ * {@link EJBClientInvocationContext#sendRequest()} is called
+ *
+ * @author Jaikiran Pai
+ */
+//@AT({"modules/testcases/jdkAll/Wildfly/ejb/src/main/java#27.0.0.Alpha2*27.0.0.Alpha3"})
+public class SimpleEJBClientInterceptor implements EJBClientInterceptor {
+
+    private final Map<String, Object> data;
+
+    SimpleEJBClientInterceptor(final Map<String, Object> data) {
+        this.data = data;
+    }
+
+    @Override
+    public void handleInvocation(EJBClientInvocationContext context) throws Exception {
+        // add all the data to the EJB client invocation context so that it becomes available to the server side
+        context.getContextData().put("AnyKey", "AnyValue");
+        System.out.println("Added context data!");
+        // proceed "down" the invocation chain
+        context.sendRequest();
+    }
+
+    @Override
+    public Object handleInvocationResult(EJBClientInvocationContext context) throws Exception {
+        // we don't have anything special to do with the result so just return back the result
+        // "up" the invocation chain
+        System.out.println("Rcontext " + context.getContextData());
+        return context.getResult();
+    }
+}
