e6380918afce59a8259f844aa1f194dd826e63fa
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-08-05 21:02:10 +0300

    Adding some more jk tests  ...

--- modules/src1/main/java/org/jboss/additional/testsuite/jdkall/present/logging/misc/jakarta/JaxRsActivator.java
+++ modules/src1/main/java/org/jboss/additional/testsuite/jdkall/present/logging/misc/jakarta/JaxRsActivator.java
@@ -21,13 +21,13 @@ package org.jboss.additional.testsuite.jdkall.present.logging.misc;
 
 import org.jboss.eap.additional.testsuite.annotations.EAT;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 /**
  * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
  */
-@EAT({"modules/testcases/jdkAll/WildflyRelease-13.0.0.Final/logging/src/main/java","modules/testcases/jdkAll/WildflyRelease-24.0.0.Final/logging/src/main/java","modules/testcases/jdkAll/Wildfly/logging/src/main/java","modules/testcases/jdkAll/WildflyJakarta/logging/src/main/java#27.0.0.Alpha4","modules/testcases/jdkAll/ServerBeta/logging/src/main/java","modules/testcases/jdkAll/WildflyRelease-17.0.0.Final/logging/src/main/java","modules/testcases/jdkAll/WildflyRelease-20.0.0.Final/logging/src/main/java","modules/testcases/jdkAll/Eap72x/logging/src/main/java","modules/testcases/jdkAll/Eap72x-Proposed/logging/src/main/java","modules/testcases/jdkAll/Eap7Plus/logging/src/main/java","modules/testcases/jdkAll/Eap71x-Proposed/logging/src/main/java","modules/testcases/jdkAll/Eap71x/logging/src/main/java","modules/testcases/jdkAll/Eap70x/logging/src/main/java"})
+@EAT({"modules/testcases/jdkAll/WildflyJakarta/logging/src/main/java#27.0.0.Alpha4"})
 @ApplicationPath("/rest")
 public class JaxRsActivator extends Application {
 }
