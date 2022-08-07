c072e7dfc639b48510c050a70eb43c62987bb162
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-08-07 17:07:37 +0300

    Adding some more jk tests  ...

---
+++ modules/src1/main/java/org/jboss/additional/testsuite/jdkall/present/management/cli/jakarta/ReplacedLibraryServlet.java
@@ -0,0 +1,22 @@
+package org.jboss.additional.testsuite.jdkall.present.management.cli;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.jboss.eap.additional.testsuite.annotations.EAT;
+
+/**
+ * @author Stuart Douglas
+ */
+@WebServlet(urlPatterns = "/LibraryServlet")
+@EAT({"modules/testcases/jdkAll/WildflyJakarta/management/src/main/java#27.0.0.Alpha4"})
+public class ReplacedLibraryServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Replaced Library Servlet");
+    }
+}
