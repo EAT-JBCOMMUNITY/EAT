4f49e4b32802ee45e7469938ee80267bd39c63ab
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-08-09 22:54:23 +0300

    Adding some more jk tests  ...

--- modules/src1/main/java/org/hibernate/jmx/jakarta/StatisticsService.java
+++ modules/src1/main/java/org/hibernate/jmx/jakarta/StatisticsService.java
@@ -20,10 +20,21 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-package org.springframework.jacksontest;
+package org.hibernate.jmx;
 
+import jakarta.xml.bind.annotation.XmlRootElement;
 import org.jboss.eap.additional.testsuite.annotations.EAT;
 
-@EAT({"modules/testcases/jdkAll/OpenLiberty/jaxrs/src/main/java","modules/testcases/jdkAll/WildflyRelease-13.0.0.Final/jaxrs/src/main/java","modules/testcases/jdkAll/ServerBeta/jaxrs/src/main/java","modules/testcases/jdkAll/WildflyRelease-17.0.0.Final/jaxrs/src/main/java","modules/testcases/jdkAll/WildflyRelease-24.0.0.Final/jaxrs/src/main/java","modules/testcases/jdkAll/Wildfly/jaxrs/src/main/java","modules/testcases/jdkAll/WildflyRelease-20.0.0.Final/jaxrs/src/main/java","modules/testcases/jdkAll/Eap72x/jaxrs/src/main/java","modules/testcases/jdkAll/Eap72x-Proposed/jaxrs/src/main/java","modules/testcases/jdkAll/Eap7Plus/jaxrs/src/main/java","modules/testcases/jdkAll/Eap71x/jaxrs/src/main/java","modules/testcases/jdkAll/Eap71x-Proposed/jaxrs/src/main/java","modules/testcases/jdkAll/Eap70x/jaxrs/src/main/java","modules/testcases/jdkAll/WildflyJakarta/jaxrs/src/main/java#27.0.0.Alpha4"})
-public class AbstractApplicationContext {
+@EAT({"modules/testcases/jdkAll/WildflyJakarta/jaxrs/src/main/java#27.0.0.Alpha4"})
+@XmlRootElement
+public class StatisticsService {
+    private String sessionFactoryJNDIName;
+
+    public String getSessionFactoryJNDIName() {
+        return sessionFactoryJNDIName;
+    }
+
+    public void setSessionFactoryJNDIName(String sessionFactoryJNDIName) {
+        this.sessionFactoryJNDIName = sessionFactoryJNDIName;
+    }
 }
--- modules/src1/main/java/org/springframework/jacksontest/jakarta/BogusApplicationContext.java
+++ modules/src1/main/java/org/springframework/jacksontest/jakarta/BogusApplicationContext.java
@@ -24,6 +24,9 @@ package org.springframework.jacksontest;
 
 import org.jboss.eap.additional.testsuite.annotations.EAT;
 
-@EAT({"modules/testcases/jdkAll/OpenLiberty/jaxrs/src/main/java","modules/testcases/jdkAll/WildflyRelease-13.0.0.Final/jaxrs/src/main/java","modules/testcases/jdkAll/ServerBeta/jaxrs/src/main/java","modules/testcases/jdkAll/WildflyRelease-17.0.0.Final/jaxrs/src/main/java","modules/testcases/jdkAll/WildflyRelease-24.0.0.Final/jaxrs/src/main/java","modules/testcases/jdkAll/Wildfly/jaxrs/src/main/java","modules/testcases/jdkAll/WildflyRelease-20.0.0.Final/jaxrs/src/main/java","modules/testcases/jdkAll/Eap72x/jaxrs/src/main/java","modules/testcases/jdkAll/Eap72x-Proposed/jaxrs/src/main/java","modules/testcases/jdkAll/Eap7Plus/jaxrs/src/main/java","modules/testcases/jdkAll/Eap71x/jaxrs/src/main/java","modules/testcases/jdkAll/Eap71x-Proposed/jaxrs/src/main/java","modules/testcases/jdkAll/Eap70x/jaxrs/src/main/java","modules/testcases/jdkAll/WildflyJakarta/jaxrs/src/main/java#27.0.0.Alpha4"})
-public class AbstractApplicationContext {
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@EAT({"modules/testcases/jdkAll/WildflyJakarta/jaxrs/src/main/java#27.0.0.Alpha4"})
+@XmlRootElement
+public class BogusApplicationContext extends AbstractApplicationContext {
 }
