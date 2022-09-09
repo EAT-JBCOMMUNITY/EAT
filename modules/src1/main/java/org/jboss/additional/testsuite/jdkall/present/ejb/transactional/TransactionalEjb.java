088bc89e10e0279fcc712f9a795e47b5c9cf2693
Panagiotis Sotiropoulos <psotirop@redhat.com>
2022-09-09 09:34:13 +0200

    Adding test for WFLY-14769,JBEAP-22637 ...

---
+++ modules/src1/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/transactional/TransactionalEjb.java
@@ -0,0 +1,45 @@
+package org.jboss.additional.testsuite.jdkall.present.ejb.transactional;
+
+import java.util.Properties;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import static javax.ejb.TransactionAttributeType.NOT_SUPPORTED;
+import static javax.ejb.TransactionAttributeType.REQUIRED;
+import javax.naming.InitialContext;
+import javax.transaction.UserTransaction;
+import org.jboss.eap.additional.testsuite.annotations.EAT;
+
+@EAT({"modules/testcases/jdkAll/Wildfly/ejb/src/main/java#28.0.0","modules/testcases/jdkAll/Eap7Plus/ejb/src/main/java#7.4.8"})
+@Stateless
+public class TransactionalEjb {
+
+    @TransactionAttribute(NOT_SUPPORTED)
+    public boolean notSupportedTransactional() throws Exception {
+        boolean success = false;
+        Properties props = new Properties();
+        props.put("java.naming.factory.initial", "org.wildfly.naming.client.WildFlyInitialContextFactory");
+        props.put("java.naming.provider.url", "remote://localhost:4447");
+        props.put("java.naming.factory.url.pkgs", "org.jboss.naming:org.jnp.interfaces");
+        InitialContext ctx = new InitialContext(props);
+        UserTransaction tx = (UserTransaction) ctx.lookup("txn:LocalUserTransaction");
+        tx.begin();
+        success = true;
+        tx.commit();
+        return success;
+    }
+
+    @TransactionAttribute(REQUIRED)
+    public boolean requiredTransactional() throws Exception {
+        boolean success = false;
+        Properties props = new Properties();
+        props.put("java.naming.factory.initial", "org.wildfly.naming.client.WildFlyInitialContextFactory");
+        props.put("java.naming.provider.url", "remote://localhost:4447");
+        props.put("java.naming.factory.url.pkgs", "org.jboss.naming:org.jnp.interfaces");
+        InitialContext ctx = new InitialContext(props);
+        UserTransaction tx = (UserTransaction) ctx.lookup("txn:LocalUserTransaction");
+        tx.begin();
+        tx.commit();
+        success = true;
+        return success;
+    }
+}
