c47b8fad128a7a198236b575d7d64a1565a4efbd
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-09-29 22:38:55 +0300

    src2 is no longer used   ...

--- modules/src2/main/java/com/mchange/v2/c3p0/jacksonTest/ComboPooledDataSource.java
+++
@@ -1,8 +0,0 @@
-package com.mchange.v2.c3p0.jacksonTest;
-
-import org.jboss.eap.additional.testsuite.annotations.EAT;
-
-@EAT({"modules/testcases/jdkAll/WildflyRelease-13.0.0.Final/jaxrs/src/main/java","modules/testcases/jdkAll/ServerBeta/jaxrs/src/main/java","modules/testcases/jdkAll/WildflyRelease-17.0.0.Final/jaxrs/src/main/java","modules/testcases/jdkAll/Wildfly/jaxrs/src/main/java","modules/testcases/jdkAll/WildflyRelease-20.0.0.Final/jaxrs/src/main/java","modules/testcases/jdkAll/Eap72x/jaxrs/src/main/java","modules/testcases/jdkAll/Eap72x-Proposed/jaxrs/src/main/java","modules/testcases/jdkAll/Eap7Plus/jaxrs/src/main/java","modules/testcases/jdkAll/Eap71x-Proposed/jaxrs/src/main/java","modules/testcases/jdkAll/Eap71x/jaxrs/src/main/java"})
-public class ComboPooledDataSource {
-
-}
--- modules/src2/main/java/org/jboss/additional/testsuite/jdkall/future/README.md
+++
@@ -1,6 +0,0 @@
-# EAP ADDITIONAL TESTSUITE
-==========================
-## THE JBOSS TESTSUITE TO DEVELOP TESTS AGAINST INFINITE NUMBER OF JBOSS SERVERS
-================================================================================
-
-In this directory should be included the tests that demonstrate the features of the FUTURE JBoss Servers.
