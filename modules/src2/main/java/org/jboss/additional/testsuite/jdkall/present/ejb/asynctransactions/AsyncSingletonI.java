4645521062e0ad578533fcec624b3f6c57e33e10
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-09-29 22:38:55 +0300

    src2 is no longer used   ...

--- modules/src2/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/asynctransactions/AsyncSingletonI.java
+++
@@ -1,8 +0,0 @@
-package org.jboss.additional.testsuite.jdkall.present.ejb.asynctransactions;
-
-import org.jboss.eap.additional.testsuite.annotations.EAT;
-
-@EAT({"modules/testcases/jdkAll/Eap72x/ejb/src/main/java#7.2.7","modules/testcases/jdkAll/Eap72x-Proposed/ejb/src/main/java#7.2.7","modules/testcases/jdkAll/Eap7Plus/ejb/src/main/java#7.3.0.GA","modules/testcases/jdkAll/Wildfly/ejb/src/main/java#19.0.0.Beta1"})
-public interface AsyncSingletonI {
-    void doAsyncAction();
-}
