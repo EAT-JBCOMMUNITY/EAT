4f923a63fc78edf225ae3605e958ec9149df1b5a
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-10-04 00:30:27 +0300

    Removing redundant files  ...

--- modules/testcases/jdkAll/Wildfly/ejb/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/sfsbconcurrency/Foo.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.additional.testsuite.jdkall.present.ejb.sfsbconcurrency;
-import org.jboss.eap.additional.testsuite.annotations.EAT;
-
-@EAT({"modules/testcases/jdkAll/WildflyRelease-24.0.0.Final/ejb/src/main/java","modules/testcases/jdkAll/Wildfly/ejb/src/main/java","modules/testcases/jdkAll/ServerBeta/ejb/src/main/java","modules/testcases/jdkAll/WildflyRelease-17.0.0.Final/ejb/src/main/java","modules/testcases/jdkAll/Eap72x/ejb/src/main/java","modules/testcases/jdkAll/Eap72x-Proposed/ejb/src/main/java","modules/testcases/jdkAll/Eap7Plus/ejb/src/main/java","modules/testcases/jdkAll/WildflyJakarta/ejb/src/main/java#27.0.0.Alpha4","modules/testcases/jdkAll/WildflyRelease-27.0.0.Final/ejb/src/main/java"})
-public class Foo {
-    public void foo() {
-           
-	}
-}
--- modules/testcases/jdkAll/Wildfly/security/src/main/java/com/caucho/config/types/ResourceRef.java
+++
@@ -1,7 +0,0 @@
-package com.caucho.config.types;
-
-import org.jboss.eap.additional.testsuite.annotations.EAT;
-
-@EAT({"modules/testcases/jdkAll/WildflyRelease-24.0.0.Final/security/src/main/java","modules/testcases/jdkAll/Wildfly/security/src/main/java#20.0.0.Final","modules/testcases/jdkAll/ServerBeta/security/src/main/java#20.0.0.Final","modules/testcases/jdkAll/WildflyRelease-20.0.0.Final/security/src/main/java#20.0.0.Final","modules/testcases/jdkAll/Eap72x/security/src/main/java#7.2.9","modules/testcases/jdkAll/Eap72x-Proposed/security/src/main/java#7.2.9","modules/testcases/jdkAll/Eap7Plus/security/src/main/java#7.3.2"})
-public class ResourceRef {
-}
