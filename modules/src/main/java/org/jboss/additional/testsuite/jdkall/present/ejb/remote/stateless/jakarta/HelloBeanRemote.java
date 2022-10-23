538490f0397a03b7063308f484fa53aa42f3932a
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-10-24 02:51:10 +0300

    Source distribution update  ...

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/remote/stateless/jakarta/HelloBeanRemote.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/remote/stateless/jakarta/HelloBeanRemote.java
@@ -4,7 +4,7 @@ import jakarta.ejb.Remote;
 import org.jboss.eap.additional.testsuite.annotations.EAT;
 
 @Remote
-@EAT({"modules/testcases/jdkAll/WildflyJakarta/ejb/src/main/java#27.0.0.Final"})
+@EAT({"modules/testcases/jdkAll/WildflyJakarta/ejb/src/main/java#27.0.0.Final","modules/testcases/jdkAll/WildflyRelease-27.0.0.Final/ejb/src/main/java"})
 public interface HelloBeanRemote {
 
     public String hello();
