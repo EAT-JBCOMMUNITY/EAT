ad7c807097f366a694d21c37cdc91f7d27531a65
Panagiotis Sotiropoulos <panossoto@gmail.com>
2022-11-14 00:11:53 +0200

    Update ServletWebConfigTestCase.java

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/web/servlet/ServletWebConfigTestCase.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/web/servlet/ServletWebConfigTestCase.java
@@ -37,7 +37,7 @@ import java.net.URL;
 
 @RunAsClient
 @RunWith(Arquillian.class)
-@EAT({"modules/testcases/jdkAll/Wildfly/web/src/main/java#28.0.0","modules/testcases/jdkAll/Eap7Plus/web/src/main/java#7.4.9"})
+@EAT({"modules/testcases/jdkAll/WildflyJakarta/web/src/main/java#29.0.0","modules/testcases/jdkAll/Eap7Plus/web/src/main/java#7.4.9"})
 public class ServletWebConfigTestCase {
 
     private static final String SERVLET_DEPLOYMENT = "servletwebconfig";
