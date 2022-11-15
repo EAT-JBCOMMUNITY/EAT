d425c4f68e524379833089941c2b100b57006f03
Panagiotis Sotiropoulos <psotirop@redhat.com>
2022-11-15 10:27:25 +0100

    Adding a test for JBEAP-21737, JBEAP-21738, WFLY(ELYWEB-133) ...

---
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/elytron/login/MyServletExtension.java
@@ -0,0 +1,24 @@
+package org.jboss.additional.testsuite.jdkall.present.elytron.login;
+
+import javax.servlet.ServletContext;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.servlet.ServletExtension;
+import io.undertow.servlet.api.DeploymentInfo;
+import org.jboss.eap.additional.testsuite.annotations.EAT;
+
+@EAT({"modules/testcases/jdkAll/Eap7Plus/elytron/src/main/java#7.4.8"})
+public class MyServletExtension
+        implements ServletExtension {
+
+    private static final HttpHandler myTokenHandler = new MyDummyTokenHandler();
+
+    @Override
+    public void handleDeployment(DeploymentInfo deploymentInfo, ServletContext servletContext) {
+        deploymentInfo.addSecurityWrapper(nextHandler -> exchange -> {
+            myTokenHandler.handleRequest(exchange);
+            nextHandler.handleRequest(exchange);
+        });
+    }
+
+}
