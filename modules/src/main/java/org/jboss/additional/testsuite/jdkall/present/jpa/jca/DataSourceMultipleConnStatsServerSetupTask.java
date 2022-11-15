931d1099d1f64edc7328988e365e4d88186e5982
Panagiotis Sotiropoulos <psotirop@redhat.com>
2022-11-15 12:08:08 +0100

    Adding a test for JBEAP-21012, JBEAP-21295, WFLY-14691 ...

---
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/jpa/jca/DataSourceMultipleConnStatsServerSetupTask.java
@@ -0,0 +1,75 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.additional.testsuite.jdkall.present.jpa.jca;
+
+import org.jboss.as.arquillian.container.ManagementClient;
+import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
+import org.jboss.as.controller.operations.common.Util;
+import org.jboss.as.test.shared.ServerReload;
+import org.jboss.as.test.integration.security.common.CoreUtils;
+import org.jboss.as.test.shared.SnapshotRestoreSetupTask;
+import org.jboss.dmr.ModelNode;
+
+import java.util.List;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ALLOW_RESOURCE_SERVICE_RESTART;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OPERATION_HEADERS;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ROLLBACK_ON_RUNTIME_FAILURE;
+import static org.jboss.as.test.integration.management.util.ModelUtil.createOpNode;
+
+/**
+ *
+ * Server setup task for the test case DataSourceMultipleConnStatsTestCase.
+ * Enables statistics and sets min/max pool size on the ExampleDS datasource.
+ *
+ * @author Daniel Cihak
+ */
+import org.jboss.eap.additional.testsuite.annotations.EAT;
+
+@EAT({"modules/testcases/jdkAll/Eap7Plus/jpa/src/main/java#7.4.8","modules/testcases/jdkAll/WildflyJakarta/jpa/src/main/java#28.0.0"})
+public class DataSourceMultipleConnStatsServerSetupTask extends SnapshotRestoreSetupTask {
+
+    @Override
+    public void doSetup(ManagementClient managementClient, String s) throws Exception {
+
+        ModelNode enableStatsOp = createOpNode("subsystem=datasources/data-source=ExampleDS", ModelDescriptionConstants.WRITE_ATTRIBUTE_OPERATION);
+        enableStatsOp.get(ModelDescriptionConstants.NAME).set("statistics-enabled");
+        enableStatsOp.get(ModelDescriptionConstants.VALUE).set(true);
+
+        ModelNode maxPoolSizeOp = createOpNode("subsystem=datasources/data-source=ExampleDS", ModelDescriptionConstants.WRITE_ATTRIBUTE_OPERATION);
+        maxPoolSizeOp.get(ModelDescriptionConstants.NAME).set("max-pool-size");
+        maxPoolSizeOp.get(ModelDescriptionConstants.VALUE).set(1);
+
+        ModelNode minPoolSizeOp = createOpNode("subsystem=datasources/data-source=ExampleDS", ModelDescriptionConstants.WRITE_ATTRIBUTE_OPERATION);
+        minPoolSizeOp.get(ModelDescriptionConstants.NAME).set("min-pool-size");
+        minPoolSizeOp.get(ModelDescriptionConstants.VALUE).set(1);
+
+        ModelNode updateOp = Util.createCompositeOperation(List.of(enableStatsOp, maxPoolSizeOp, minPoolSizeOp));
+        updateOp.get(OPERATION_HEADERS, ROLLBACK_ON_RUNTIME_FAILURE).set(false);
+        updateOp.get(OPERATION_HEADERS, ALLOW_RESOURCE_SERVICE_RESTART).set(true);
+        CoreUtils.applyUpdate(updateOp, managementClient.getControllerClient());
+
+        ServerReload.reloadIfRequired(managementClient);
+    }
+
+}
