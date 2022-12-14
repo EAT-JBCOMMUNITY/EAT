4a88853318cc39426e7dbc6a0b2f9e2bc9e64c5e
Panagiotis Sotiropoulos <psotirop@redhat.com>
2022-12-14 15:40:26 +0100

    Fixing some permission issues during distribution ...

---
+++ modules/testcases/jdkAll/Eap7Plus/ejb/test2-configurations/startServer.sh
@@ -0,0 +1,3 @@
+# @EAT({"modules/testcases/jdkAll/Eap72x/ejb/test2-configurations","modules/testcases/jdkAll/Eap72x-Proposed/ejb/test2-configurations","modules/testcases/jdkAll/Eap71x/ejb/test2-configurations","modules/testcases/jdkAll/Eap71x-Proposed/ejb/test2-configurations","modules/testcases/jdkAll/Eap7Plus/ejb/test2-configurations"}) 
+
+./../../../../../servers/eap7/build/target/jbossas/bin/standalone.sh>output.txt
