67e07e81fe8ca061c3bb8a9878a01273364cad1e
Panagiotis Sotiropoulos <psotirop@redhat.com>
2023-01-11 17:35:04 +0100

    Adding a test for JBEAP-24375 which can be extended to other servers ...

---
+++ modules/src/main/java/com/landg/cnbs/web/common/WebConstants.java
@@ -0,0 +1,8 @@
+package com.landg.cnbs.web.common;
+
+import org.jboss.eap.additional.testsuite.annotations.EAT;
+
+@EAT({"modules/testcases/jdkAll/WildflyJakarta/web/src/main/java#28.0.0","modules/testcases/jdkAll/Eap7Plus/web/src/main/java#7.4.9"})
+public class WebConstants {
+    public static final String REFRESH = "WEBCONSTANTS_REFRESH";
+}
