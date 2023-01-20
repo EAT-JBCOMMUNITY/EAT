2e984678477baacf8b573f827bfdb5644d355c16
Panagiotis Sotiropoulos <psotirop@redhat.com>
2023-01-20 13:20:32 +0100

    Adding server side resteasy filter examples ...

---
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/jaxrs/filters/ResponseFilter.java
@@ -0,0 +1,23 @@
+package org.jboss.additional.testsuite.jdkall.present.jaxrs.filters;
+
+import java.io.IOException;
+
+import org.jboss.eap.additional.testsuite.annotations.EAT;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@EAT({"modules/testcases/jdkAll/Eap7Plus/jaxrs/src/main/java"})
+@Provider
+public class ResponseFilter implements  ContainerResponseFilter {
+    
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+    	 System.out.println("ResponseFilter ... ");
+    	if (requestContext.getMethod().equals("GET")) {    		 
+    		responseContext.getHeaders().add("Content-Length", "348");
+    	}
+    }
+	 
+}
