1c063643d19646849cba041c7ba19b66e1ad19b0
Panagiotis Sotiropoulos <psotirop@redhat.com>
2023-01-24 11:52:33 +0100

    Extending the unit test for JBEAP-24421 to an arquillian server test for multiple versions ...

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/web/jsp/JspJastowTestCase.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/web/jsp/JspJastowTestCase.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.apache.http.util.EntityUtils;
 import java.net.URL;
 import java.util.concurrent.TimeoutException;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -33,7 +33,7 @@ public class JspJastowTestCase {
 
     private static final Logger log = LoggerFactory.getLogger(JspJastowTestCase.class);
 
-     private static final String PAGE_CONTENT = "<%@ taglib prefix=\"c\" uri=\"http://java.sun.com/jsp/jstl/core\" %>"
+    private static final String PAGE_CONTENT = "<%@ taglib prefix=\"c\" uri=\"http://java.sun.com/jsp/jstl/core\" %>"
 	+ "<%@ page language=\"java\" contentType=\"text/html; charset=UTF-8\" pageEncoding=\"ISO-8859-1\"%>"
 	+ "<%@ page import=\"com.landg.cnbs.web.common.WebConstants\" %>"
 	+ "<html>"
@@ -49,6 +49,22 @@ public class JspJastowTestCase {
 	+ "</div>"
 	+ "</body>"
 	+ "</html>";
+	
+     private static final String PAGE_CONTENT2 = "<%@ page language=\"java\" contentType=\"text/html;charset=UTF-8\" pageEncoding=\"UTF-8\"%>"
+	+ "<ul>"
+        + "<li>euro=${param.euro}</li>"
+        + "<li>acutes=${param.acutes}</li>"
+        + "</ul>";
+	
+    private static final String PAGE_CONTENT3 = "<%@ page language=\"java\" contentType=\"text/html;charset=UTF-8\" pageEncoding=\"UTF-8\"%>" 
+	+ "<html>"
+	+ "    <body>"
+	+ "	<jsp:include page=\"/include.jsp\">"
+	+ "	    <jsp:param name=\"euro\" value=\"€\" />"
+	+ "	    <jsp:param name=\"acutes\" value=\"áéíóú\" />"
+	+ "	</jsp:include>"
+	+ "    </body>"
+	+ "</html>";
 
      private static final String WEBXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
             + "<web-app xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://java.sun.com/xml/ns/javaee\" xsi:schemaLocation=\"http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_2_5.xsd\" id=\"WebApp_ID\" version=\"2.5\">\n"
@@ -59,6 +75,8 @@ public class JspJastowTestCase {
         WebArchive war = ShrinkWrap.create(WebArchive.class, JSP_DEPLOYMENT + ".war");
         war.addClass(com.landg.cnbs.web.common.WebConstants.class);
         war.add(new StringAsset(PAGE_CONTENT), "jspjastow.jsp");
+        war.add(new StringAsset(PAGE_CONTENT2), "include.jsp");
+        war.add(new StringAsset(PAGE_CONTENT3), "include_parent.jsp");
         war.addAsWebInfResource(new StringAsset(WEBXML), "web.xml");
         log.info(war.toString(true));
         return war;
@@ -85,5 +103,32 @@ public class JspJastowTestCase {
             log.warn("The request threw an exception", e);
         }
     }
+    
+    @Test
+    public void jspJastowUTF8Test(@ArquillianResource URL url) throws InterruptedException, TimeoutException {
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create()
+                .setMaxConnPerRoute(1)
+                .setMaxConnTotal(1)
+                .setRetryHandler(new DefaultHttpRequestRetryHandler(0, false));
+        try (CloseableHttpClient httpClient = httpClientBuilder.build()) {
+            HttpGet httpGet = new HttpGet(url.toExternalForm() + "include_parent.jsp");
+            CloseableHttpResponse response = null;
+            try {
+                log.info("Performing request to: " + httpGet.toString());
+                response = httpClient.execute(httpGet);
+                String content = EntityUtils.toString(response.getEntity());
+             //   System.out.println("========== " + content);
+
+                Assert.assertTrue(response.getStatusLine().getStatusCode() == 200);
+                Assert.assertTrue(content.contains("euro=€"));
+                Assert.assertTrue(content.contains("acutes=áéíóú"));
+            } catch (HttpHostConnectException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            log.warn("The request threw an exception", e);
+        }
+    }
+
 
 }
