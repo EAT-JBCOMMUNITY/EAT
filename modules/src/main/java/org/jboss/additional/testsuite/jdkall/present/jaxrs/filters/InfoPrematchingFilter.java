5b6b2026af8bcc50de89bfb0fa84ded5c2136a05
Panagiotis Sotiropoulos <psotirop@redhat.com>
2023-01-27 13:26:37 +0100

    Adding example testcases for resteasy prematching filters ...

---
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/jaxrs/filters/InfoPrematchingFilter.java
@@ -0,0 +1,25 @@
+package org.jboss.additional.testsuite.jdkall.present.jaxrs.filters;
+
+import org.jboss.eap.additional.testsuite.annotations.EAT;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.container.PreMatching;
+
+
+@EAT({"modules/testcases/jdkAll/Eap7Plus/jaxrs/src/main/java"})
+@Provider 
+@PreMatching
+public class InfoPrematchingFilter implements ContainerRequestFilter  {
+     
+        @Override
+        public void filter(ContainerRequestContext requestContext) throws IOException {
+            if (requestContext.getMethod().equals("POST")) {
+                requestContext.setMethod("GET");
+            }
+            System.out.println("Prematching RequestFilter ... ");
+        }	 
+}
