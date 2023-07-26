b41ba78af811b195219002222647cacc7b12a07f
Panagiotis Sotiropoulos <psotirop@redhat.com>
2023-07-26 11:10:12 +0200

    Adding a server testcase for JBEAP-22735 ...

---
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/web/jndi/Processing.java
@@ -0,0 +1,53 @@
+package org.jboss.additional.testsuite.jdkall.present.web.jndi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Properties;
+
+import javax.naming.InitialContext;
+import javax.servlet.http.HttpServletResponse;
+import org.jboss.eap.additional.testsuite.annotations.EAT;
+
+@EAT({"modules/testcases/jdkAll/Eap7Plus/web/src/main/java#7.4.3"})
+public class Processing implements Runnable {
+
+    private final HttpServletResponse response;
+    private final String jndi;
+
+    public Processing(final String jndi, final HttpServletResponse response) {
+        super();
+        this.response = response;
+        this.jndi = jndi;
+    }
+
+    public void run() {
+        try {
+            final String result = lookup(jndi);
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().write(result);
+            response.getWriter().flush();
+            response.getWriter().close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_OK);
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            try {
+                response.getWriter().write(sw.toString());
+                response.getWriter().flush();
+                response.getWriter().close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    private String lookup(final String jndi) throws Exception {
+        Properties props = new Properties();
+        final String result = ((ConfigInterface) new InitialContext(props).lookup(jndi)).getConfigValue();
+        return "lookup [" + jndi + "] : " + result;
+    }
+
+}
