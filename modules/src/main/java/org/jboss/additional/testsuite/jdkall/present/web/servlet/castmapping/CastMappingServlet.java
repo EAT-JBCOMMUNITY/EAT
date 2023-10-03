20687afa01aeb50c0a13426a5f8c7594b720828c
Panagiotis Sotiropoulos <psotirop@redhat.com>
2023-10-03 10:31:36 +0200

    Adding a test for JBEAP-25455 ...

---
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/web/servlet/castmapping/CastMappingServlet.java
@@ -0,0 +1,46 @@
+
+package org.jboss.additional.testsuite.jdkall.present.web.servlet.castmapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import javax.servlet.http.HttpServletMapping;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.MappingMatch;
+import java.io.PrintWriter;
+import org.jboss.eap.additional.testsuite.annotations.EAT;
+
+@WebServlet(name = "CastMappingServlet", urlPatterns = {"/CastMappingServlet",
+            "/AForwardToB",
+            "/AIncludesB"})
+@EAT({"modules/testcases/jdkAll/Eap7Plus/web/src/main/java#7.4.14"})
+public class CastMappingServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	PrintWriter out = response.getWriter();
+	printCurrentMappingDetails(request, out);
+        HttpServletMapping mapping = request.getHttpServletMapping();
+        if (mapping.getPattern().equals("/CastMappingServlet")) {
+            RequestDispatcher rd = request.getRequestDispatcher("/AIncludesB");
+            rd.include(request, response);
+        }
+    }
+    
+    public static void printCurrentMappingDetails(HttpServletRequest request,
+            PrintWriter out) throws IOException {
+            HttpServletMapping forwardMapping = (HttpServletMapping) request.getAttribute(RequestDispatcher.FORWARD_MAPPING);
+            HttpServletMapping includeMapping = (HttpServletMapping) request.getAttribute(RequestDispatcher.INCLUDE_MAPPING);
+
+            out.print("<p> " + request.getHttpServletMapping() + "</p>");
+            out.print("<p> FORWARD_MAPPING: " + forwardMapping + "</p>");
+            out.print("<p> INCLUDE_MAPPING: " + includeMapping + "</p>");
+            out.print("<hr />");
+
+
+    }
+   
+}
---
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/web/servlet/castmapping/CastMappingTestCase.java
@@ -0,0 +1,58 @@
+package org.jboss.additional.testsuite.jdkall.present.web.servlet.castmapping;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.Header;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.eap.additional.testsuite.annotations.EAT;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+
+@RunAsClient
+@RunWith(Arquillian.class)
+@EAT({"modules/testcases/jdkAll/Eap7Plus/web/src/main/java#7.4.14","modules/testcases/jdkAll/WildflyJakarta/web/src/main/java#31.0.0"})
+public class CastMappingTestCase {
+
+    public static final String DEPLOYMENT = "castmapping.war";
+
+    @Deployment(name = DEPLOYMENT)
+    public static Archive<?> getDeployment() {
+        WebArchive war = ShrinkWrap.create(WebArchive.class, DEPLOYMENT);
+        war.addClass(CastMappingServlet.class);
+        return war;
+    }
+
+    @Test
+    @OperateOnDeployment(DEPLOYMENT)
+    public void catMappingTest(@ArquillianResource URL url) throws Exception {
+        URL testURL = new URL(url.toString() + "CastMappingServlet");
+
+        final HttpGet request = new HttpGet(testURL.toString());
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        CloseableHttpResponse response = null;
+
+        try {
+            response = httpClient.execute(request);
+            Assert.assertEquals("Failed to access " + testURL, HttpURLConnection.HTTP_OK, response.getStatusLine().getStatusCode());        
+        } finally {
+            IOUtils.closeQuietly(response);
+            httpClient.close();
+        }
+    }
+}
