806dcce19e1684feea89cee204c4edcd3be01ecb
Panagiotis Sotiropoulos <panossoto@gmail.com>
2023-10-21 09:49:05 +0300

    Adding a vo  ...

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/web/jsp/JspKeepAliveTestCase.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/web/jsp/JspKeepAliveTestCase.java
@@ -17,7 +17,7 @@ import org.junit.Test;
 
 @RunWith(Arquillian.class)
 @RunAsClient
-@EAT({"modules/testcases/jdkAll/WildflyJakarta/web/src/main/java#31.0.0","modules/testcases/jdkAll/Eap7Plus/web/src/main/java#7.4.14"})
+@EAT({"modules/testcases/jdkAll/WildflyJakarta/web/src/main/java#32.0.0","modules/testcases/jdkAll/Eap7Plus/web/src/main/java#7.4.14"})
 public class JspKeepAliveTestCase {
 
     private static final String JSP_DEPLOYMENT = "examplejsp";
