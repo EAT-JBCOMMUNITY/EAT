e299b7d524588c85e884e7ca91d6801ea09d3791
Panagiotis Sotiropoulos <panossoto@gmail.com>
2024-02-23 00:38:39 +0200

    Minor corrections

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/clustering/concurrentexception/WebClusterTestCase.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/clustering/concurrentexception/WebClusterTestCase.java
@@ -35,9 +35,8 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.Before;
 import org.jboss.eap.additional.testsuite.annotations.EAT;
-<<<<<<< HEAD
 
-@EAT({"modules/testcases/jdkAll/Eap7Plus/clustering/src/main/java#7.4.15"})
+@EAT({"modules/testcases/jdkAll/Eap7Plus/clustering/src/main/java#7.4.15","modules/testcases/jdkAll/WildflyJakarta/clustering/src/main/java#31.0.0"})
 @RunWith(Arquillian.class)
 @RunAsClient
 public class WebClusterTestCase {
